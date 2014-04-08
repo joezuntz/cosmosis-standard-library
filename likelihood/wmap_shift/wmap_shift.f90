bc177427b93316df90e4a0e4da50a03ac1f0d240
Joe Zuntz <joezuntz@googlemail.com>
2014-04-08 17:42:56 +0100

    Updated wmap-shift (first peak position only) likelihood to cosmosis

--- likelihood/wmap_shift/wmap_shift.f90
+++ likelihood/wmap_shift/wmap_shift.f90
@@ -3,12 +3,10 @@
 ! Reported by WMAP9 as 
 ! R = 1.728 +/- 0.016
 
-function execute(handle) result(status)
-	use f90_desglue
-	use iso_c_binding
-	integer(c_size_t), value :: handle
-	integer(c_size_t) :: package	
-	integer(c_int) :: status
+function execute(block) result(status)
+	use cosmosis_modules
+	integer(cosmosis_block), value :: block
+	integer(cosmosis_status) :: status
 	real(8) :: R, like
 
 	!Fixed parameter values for R_mu and R_sigma
@@ -18,25 +16,13 @@ function execute(handle) result(status)
 	!Error checking
 	status = 0
 
-	! Open the package
-	package = fitsfile_from_internal(handle)
-	!Check everything opened okay.  Print message if not.
-	if (package==0) then
-		write(*,*) "Could not open package in wmap_shift. Error", status
-		status=1
-		return
-	endif
-
 	!Extract the shift parameter and calculate the likelihood
 	R = 0.0
-	status = status + fits_goto_extension(package, distances_section)
-	status = status + fits_get_double_parameter(package, "CMBSHIFT", R)
+	status = datablock_get_double(block, distances_section, "cmbshift", R)
 	like = -0.5*(R - R_mu)**2/R_sigma**2
 
 	!Save the likelihood and close the file
-	status = status + fits_goto_or_create_extension(package, likelihoods_section)
-	status = status + fits_put_double_parameter(package, "R_LIKE", like, "Shift parameter likelihood")
-	status = status + close_fits_object(package)
+	status = status + datablock_put_double(block, likelihoods_section, "SHIFT_LIKE", like)
 
 	!status is the return value - any problems will be passed along
 
