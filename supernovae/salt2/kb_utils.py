12dc7af079d3e5329448d9c8593ac3d6c81eab8a
Joe Zuntz <joezuntz@googlemail.com>
2014-04-14 19:04:25 +0100

    Added cosmosisified SALT2 code by Marisa March and Kyle Barbary.

---
+++ supernovae/salt2/kb_utils.py
@@ -0,0 +1,146 @@
+from collections import OrderedDict
+import numpy as np
+
+INT_META = ['NFAKE', 'CHIP', 'NFAKE_FOUND', 'NFAKE_ACCEPT']
+STR_META = ['BAND']
+
+INT_COLS = ['NID', 'EXPNUM', 'CHIP', 'FAKEID', 'GALID', 'SAMEHOST', 'OBJID',
+            'REJECT', 'FILTER', 'NPOS0', 'NPOS1', 'NPOS2', 'NNEG0', 'NNEG1',
+            'NNEG2',  'REJECT', 'ERR', 'TILE', 'NSTAR']
+STR_COLS = ['BAND','CID']
+
+def dict_to_ndarray(d):
+    """Convert a dictionary of lists (of equal length) to a structured
+    numpy.ndarray"""
+
+    # first convert all lists to 1-d arrays, in order to let numpy
+    # figure out the necessary size of the string arrays.
+    for key in d: 
+        d[key] = np.array(d[key])
+
+    # Determine dtype of output array.
+    dtypelist = []
+    for key in d:
+        dtypelist.append((key, d[key].dtype))
+    
+    # Initialize ndarray and then fill it.
+    firstkey = d.keys()[0]
+    col_len = len(d[firstkey])
+    result = np.empty(col_len, dtype=dtypelist)
+    for key in d:
+        result[key] = d[key]
+
+    return result
+
+def read_datafile(filename, default_tablename=None, output_ndarray=False):
+    """Read a data file with this (newline-independent) format:
+
+    * Header with 'KEYWORD: value' pairs (optional) 
+    * One or more tables. The start of a new table is indicated by a
+      keyword that starts with 'NVAR'. If the format is
+      'NVAR_TABLENAME', then 'TABLENAME' is taken to be the name of the table,
+      and datalines in the table must be started with 'TABLENAME:'
+    * A keyword starting with 'VARNAMES' must directly follow the
+      'NVAR_TABLENAME' definition. 
+
+    Parameters
+    ----------
+    filename : str
+        Filename of object to read.
+    default_tablename : str
+        
+    Returns
+    -------
+    meta : OrderedDict
+        Metadata.
+    tables : dictionary of multiple `numpy.ndarray`s or dictionaries.
+        The data.
+    """
+
+    meta = OrderedDict() # initialize structure to hold metadata.
+    tables = {} # initialize structure to hold data.
+
+    infile = open(filename, 'r')
+    words = infile.read().split()
+    infile.close()
+    i = 0
+    nvar = None
+    tablename = None
+    while i < len(words):
+        word = words[i]
+
+        # If the word starts with 'NVAR', we are starting a new table.
+        if word.startswith('NVAR'):
+            nvar = int(words[i + 1])
+
+            #Infer table name. The name will be used to designate a data row.
+            if '_' in word:
+                pos = word.find('_') + 1
+                tablename = word[pos:].rstrip(':')
+            elif default_tablename is None:
+                raise ValueError('table name not given as part of NVAR keyword.'
+                                 ' You must give a `default_tablename` so that'
+                                 ' data rows can be identified in the file.')
+            else:
+                tablename = default_tablename
+
+            table = OrderedDict()
+            tables[tablename] = table
+
+            i += 2
+
+        # If the word starts with 'VARNAMES', the following `nvar` words
+        # define the column names of the table.
+        elif word.startswith('VARNAMES'):
+
+            # Check that nvar is defined and that no column names are defined
+            # for the current table.
+            if nvar is None or len(table) > 0:
+                raise Exception('NVAR must directly precede VARNAMES')
+
+            # Read the column names
+            for j in range(i + 1, i + 1 + nvar):
+                table[words[j]] = []
+            i += nvar + 1
+
+        # If the word matches the current tablename, we are reading a data row.
+        elif word.rstrip(':') == tablename:
+            for j, colname in enumerate(table.keys()):
+                table[colname].append(words[i + 1 + j])
+            i += nvar + 1
+        
+        # Otherwise, we are reading metadata or some comment
+        # If the word ends with ":", it is metadata.
+        elif word[-1] == ':':
+            name = word[:-1]  # strip off the ':'
+            if len(words) >= i + 2:
+                meta[name] = words[i + 1]
+            else: 
+                meta[name] = None
+            i += 2
+        else:
+            # It is some comment; continue onto next word.
+            i += 1
+
+    # Convert Values in metadata
+    for key, val in meta.iteritems():
+        if key in INT_META: meta[key] = int(val)
+        elif key in STR_META: pass
+        else: meta[key] = float(val)
+
+    # Convert values in columns
+    for tablename, table in tables.iteritems():
+        for key in table.keys():
+            if key in INT_COLS:
+                table[key] = [int(float(x)) for x in table[key]] # HACK
+            elif key in STR_COLS:
+                pass
+            else:
+                table[key] = [float(x) for x in table[key]]
+                
+    # Convert tables to ndarrays
+    if output_ndarray:
+        for tablename in tables.keys():
+            tables[tablename] = dict_to_ndarray(tables[tablename])
+
+    return meta, tables
---
+++ supernovae/salt2/salt2_interface.py
@@ -0,0 +1,142 @@
+import pydesglue
+import sys,os
+import numpy as np
+import cosmosis_py
+import cosmosis_py.section_names
+
+sn_params = cosmosis_py.section_names.supernova_params
+distances = cosmosis_py.section_names.distances
+likes = cosmosis_py.section_names.likelihoods
+options = cosmosis_py.block.option_section
+
+
+run_dir=os.path.split(__file__)[0] #detect path of this directory
+#utils_dir=os.path.join(run_dir,"..","sn_utils") #To get path of utils dir 
+#sys.path.append(utils_dir) #Append utils dir to path
+
+import kb_utils # K. Barbary file reading utils
+
+INT_SIG_DEFAULT=0.1     #Currently not fitting for sig_int
+int_sig=INT_SIG_DEFAULT
+
+def setup(block):
+    datadir = block.get_string(options,"dirname", default=run_dir)
+    datafile = block.get_string(options, "filename", "SALT2.FITRES")
+    datapath=os.path.join(datadir,datafile)
+
+    meta,sntab=kb_utils.read_datafile(datapath,
+        default_tablename='SN',output_ndarray=False)
+    
+    sntab=sntab['SN']
+
+    def get(name):
+        try:
+            return np.array(sntab[name.lower()])
+        except KeyError:
+            return np.array(sntab[name])
+    #Pull out the columns we need
+    z_obs = get('Z')
+    z_sig = get("ZERR")
+    c_obs = get("c")
+    sig_c = get('cERR')
+    x1_obs = get('x1')
+    sig_x1 = get('x1ERR')
+    x0_obs = get('x0')
+    sig_x0 = get('x0ERR')
+    mb_obs = get("mB")
+    sig_mb = get("mBERR")
+    covx0x1= get('COVx0x1')
+    covx0c = get('COVx0c')
+    covx1c = get('COVx1c')
+
+
+    scalefac=-2.5/(x0_obs*np.log(10.0))
+    covmbx1=covx0x1*scalefac
+    covmbc=covx0c*scalefac
+
+    covmats=[]
+    ok_covmats=[]
+    nbad = 0
+
+    for i_c in xrange(len(z_obs)):
+        covmat=np.array([[sig_mb[i_c]**2, covmbx1[i_c], covmbc[i_c]],
+                [covmbx1[i_c],sig_x1[i_c]**2,covx1c[i_c]],
+                [covmbc[i_c], covx1c[i_c], sig_c[i_c]**2]])
+
+        if (covmat.diagonal().min()>0 and np.isfinite(covmat).all() 
+            and np.linalg.eigvals(covmat).min()>0): 
+            ok_covmats.append(True)
+            covmats.append(covmat)
+        else:
+            ok_covmats.append(False)
+            nbad += 1
+    if nbad>0:
+        print "Note: the SALT2 process can produce unphysical covariance matrices"
+        print "which are not finite.  This happened for %d out of the %d supernovae in this sample."%(nbad,len(z_obs))
+        print "These will not be used.  (This is not an error, just an advisory note.)"
+
+    ok_covmats = np.array(ok_covmats)
+    z_obs=z_obs[ok_covmats]
+    z_sig=z_sig[ok_covmats]
+    c_obs=c_obs[ok_covmats]
+    x1_obs=x1_obs[ok_covmats]
+    mb_obs=mb_obs[ok_covmats]
+
+    return z_obs,z_sig,c_obs,x1_obs,mb_obs,covmats
+
+
+def likelihood(data_vec, z_model_table, mu_model_table, M0, alpha, beta):
+    
+    z_obs,z_sig,c_obs,x1_obs,mb_obs,covmats=data_vec
+
+    #calculate mu_obs from data and M0, alpha, beta
+    mu_obs=mb_obs-M0+alpha*x1_obs-beta*c_obs
+    #calculate true mu from look up table 
+    mu_theory=np.interp(z_obs,z_model_table,mu_model_table)
+    #Make array of nuisance parameters
+    psi=np.array([-1,alpha,beta])
+
+    #Build up the vector of sigma&2 for each SN
+    mu_sig_sq=np.zeros(len(z_obs))
+    for i_c in xrange(len(z_obs)):
+        fit_sig_sq=np.dot(np.dot(psi,covmats[i_c]),psi)
+        mu_sig_sq[i_c]=fit_sig_sq+int_sig**2
+
+    #Get overall error vector
+    chisquare=(mu_obs-mu_theory)**2/mu_sig_sq
+
+    #Return log likelihood = -chi^2/2
+    LogLike=-0.5*chisquare.sum()
+    return LogLike
+  
+def execute(block,config):   
+    #Get nuisance parameters
+    M0 = block[sn_params, 'M0']
+    alpha = block[sn_params, 'alpha']
+    beta = block[sn_params, 'beta']
+
+    #Get mu(z) for theory model
+    z_model_table = block[distances, 'Z']
+    mu_model_table = block[distances, 'MU']
+
+    #Make sure z is increasing
+    if z_model_table[1]<z_model_table[0]:
+        z_model_table = z_model_table[::-1]
+        mu_model_table = mu_model_table[::-1]
+
+    #calculate the log-likelihood
+    LogLike=likelihood(config,z_model_table, mu_model_table, M0, alpha, beta)
+
+    #Give a little warning about infinity and NaN errors
+    if not np.isfinite(LogLike):
+        sys.stderr.write("Non-finite LogLike in sn_SALT2chi_like\n")
+
+    #Save the result
+    block[likes, 'SN_LIKE'] = float(LogLike)
+
+    #Signal success
+    return 0
+    
+
+
+
