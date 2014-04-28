171c9a93d3240718f2e379c03796520543149478
Joe Zuntz <joezuntz@googlemail.com>
2014-04-28 13:45:15 +0100

    Add cl -> xi module from Niall MacCrann for shear

---
+++ shear/cl_to_xi/cl_to_xi_interface.py
@@ -0,0 +1,48 @@
+#coding: utf-8
+import cl_to_xi
+import numpy as np
+import cosmosis_py.section_names
+from cosmosis_py.block import option_section
+
+
+def setup(options):
+	config = {}
+	try:
+		n_theta = options[option_section, "n_theta"]
+		theta_min = options[option_section, "theta_min"]
+		theta_max = options[option_section, "theta_max"]
+		theta_min = cl_to_xi.arcmin_to_radians(theta_min)
+		theta_max = cl_to_xi.arcmin_to_radians(theta_max)
+		theta = np.logspace(np.log10(theta_min), np.log10(theta_max), n_theta)
+	except KeyError:
+		try:
+			theta = options[option_section, 'theta']
+			if np.isscalar(theta): theta = [theta]
+		except KeyError:
+			print 'invalid theta input in options file for cl_to_xi'
+			print 'need either theta = theta_1   theta_2  ... theta_n'
+			print 'or theta_min, theta_max and n_theta.'
+			print 'thetas in arcminutes'
+			raise
+	config['theta'] = theta
+	return config
+
+
+def execute(block, config):
+	thetas = config['theta']
+	n_theta = len(thetas)
+
+	section = cosmosis_py.section_names.shear_cl
+	output_section = cosmosis_py.section_names.shear_xi
+	ell = block[section, "ell"]
+	nbin = block[section, 'nbin']
+	block[output_section, "theta"] = thetas
+
+	for i in xrange(1,nbin+1):
+		for j in xrange(1,i+1):
+			name = 'bin_%d_%d'%(i,j)
+			c_ell = block[section, name]
+			xi_plus, xi_minus = cl_to_xi.cl_to_xi_plus_and_minus_extended_apodized(ell, c_ell, thetas)
+			block[output_section, "xiplus_%d_%d"%(i,j)] = xi_plus
+			block[output_section, "ximinus_%d_%d"%(i,j)] = xi_minus
+	return 0
