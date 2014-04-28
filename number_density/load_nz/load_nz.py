72d9f4f22a66012c4b5a758c459d42a5b0c5a672
Joe Zuntz <joezuntz@googlemail.com>
2014-04-28 15:39:50 +0100

    added module to load n(z) from file instead of calculating

---
+++ number_density/load_nz/load_nz.py
@@ -0,0 +1,34 @@
+import numpy as np
+import cosmosis_py.section_names
+from cosmosis_py.block import option_section
+
+def setup(options):
+	#only one parameter - filepath
+	filename = options[option_section, "filepath"]
+	data = np.loadtxt(filename).T
+	nz = len(data[0])
+	nbin = len(data)-1
+	z = data[0]
+	n_of_z = data[1:]
+
+	#Normalize n(z)
+	for col in n_of_z:
+		norm = np.trapz(col, z)
+		col/=norm
+
+	print "Found %d samples and %d bins in redshift in file %s" % (nbin, nz, filename)
+	return (nz, nbin, z, n_of_z)
+
+def execute(block, config):
+	(nz, nbin, z, n_of_z) = config
+
+	section = cosmosis_py.section_names.wl_number_density
+	block[section, 'nz'] = nz
+	block[section, 'nbin'] = nbin
+	block[section, 'z'] = z
+	for (bin, bin_n_of_z) in enumerate(n_of_z):
+		name = "bin_%d"%(bin+1)
+		block[section, name] =  bin_n_of_z
+
+	return 0
+
