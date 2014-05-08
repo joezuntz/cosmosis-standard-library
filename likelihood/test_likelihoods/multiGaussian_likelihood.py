790728352a1fd7e97d2146543e28ff21924442f5
Alessandro Manzotti <manzotti.alessandro@gmail.com>
2014-05-08 09:59:04 -0500

    test likelihoods new cosmosis style

--- likelihood/test_likelihoods/multiGaussian_likelihood.py
+++ likelihood/test_likelihoods/multiGaussian_likelihood.py
@@ -1,26 +1,32 @@
+"""
+MultiGaussian_likelihood.py: a test problem for our samplers.
+We sample a 10 dimensional gaussian.
+"""
 import os
-import pydesglue
 import numpy as np
 import os.path
-try:
-    import pylab
-except ImportError:
-    pylab=None
+from cosmosis.datablock import names as section_names
+from cosmosis.datablock import option_section
+
+
+cosmo = section_names.cosmological_parameters
+likes = section_names.likelihoods
 
 
 class Multigaussian(object):
-    def __init__(self,ndim):
+
+    def __init__(self, ndim):
         self.ndim = ndim
         if os.path.isfile('./means10D.out'):
-            self.means=np.loadtxt('./means10D.out')
-            #print "loading saved means"
+            self.means = np.loadtxt('./means10D.out')
+            # print "loading saved means"
         # else:
         #     self.means = np.random.rand(ndim)
         #     np.savetxt('./means10D.out', self.means)
-        
+
         if os.path.isfile('./covariance10D.out'):
-            cov=np.loadtxt('./covariance10D.out')
-            #print "loading saved covariance matrix"
+            cov = np.loadtxt('./covariance10D.out')
+            # print "loading saved covariance matrix"
 
         # else:
         #     cov = 0.5-np.random.rand(ndim**2).reshape((ndim, ndim))
@@ -30,49 +36,52 @@ class Multigaussian(object):
         #     np.savetxt('covariance10D.out', cov)
 
         self.icov = np.linalg.inv(cov)
-        self.det=np.linalg.det(cov)
-        self.norm = 0.5*np.log((2.0*np.pi)**self.ndim * self.det)
+        self.det = np.linalg.det(cov)
+        self.norm = 0.5 * np.log((2.0 * np.pi) ** self.ndim * self.det)
 
-    def lnprob(self,x):
+    def lnprob(self, x):
 
-        diff = x-self.means
+        diff = x - self.means
 
-        return -np.dot(diff,np.dot(self.icov,diff))/2.0 - self.norm
+        return -np.dot(diff, np.dot(self.icov, diff)) / 2.0 - self.norm
 
-    def __call__(self,x):
-        return lnprob(self,x)
+    def __call__(self, x):
+        return lnprob(self, x)
 
 
-def execute(handle):
-    try:
-        package = pydesglue.DesDataPackage.from_fits_handle(handle)
-        section = pydesglue.section_names.cosmological_parameters
-        x=[]
-        x.append(package.get_param(section,"MG1"))
-        x.append(package.get_param(section,"MG2"))
-        x.append(package.get_param(section,"MG3"))
-        x.append(package.get_param(section,"MG4"))
-        x.append(package.get_param(section,"MG5"))
-        x.append(package.get_param(section,"MG6"))
-        x.append(package.get_param(section,"MG7"))
-        x.append(package.get_param(section,"MG8"))
-        x.append(package.get_param(section,"MG9"))
-        x.append(package.get_param(section,"MG10"))
+def setup(options):
+    section = option_section
+    return 1
+
+
+def execute(block, config):
+        x = []
+        x.append(block[cosmo, "MG1"])
+        x.append(block[cosmo, "MG1"])
+        x.append(block[cosmo, "MG1"])
+        x.append(block[cosmo, "MG1"])
+        x.append(block[cosmo, "MG1"])
+        x.append(block[cosmo, "MG1"])
+        x.append(block[cosmo, "MG1"])
+        x.append(block[cosmo, "MG1"])
+        x.append(block[cosmo, "MG1"])
+        x.append(block[cosmo, "MG1"])
         mg = Multigaussian(len(x))
-        #compute likelihood
-        like =mg.lnprob(x)
-    
-        section = pydesglue.section_names.likelihoods
-        package.set_param(section,"MULTIGAUSSIAN_LIKE",like)
-        package.write_to_fits_handle(handle)
-    
+        # compute likelihood
+        like = mg.lnprob(x)
+        block[likes, "MULTIGAUSSIAN_LIKE"] = like
+
     except KeyboardInterrupt:
-        #If the user presses Ctrl-C we respect that
+        # If the user presses Ctrl-C we respect that
         raise KeyboardInterrupt
     except Exception as E:
-        #But any other kind of error is a problem
+        # But any other kind of error is a problem
         print "There was an error calculating the MultiGaussian likelihood: "
         return 1
-        
+
         return 0
 
+def cleanup(config):
+# nothing to do here!  We just include this
+# for completeness
+    return 0
--- likelihood/test_likelihoods/rosenbrock_likelihood.py
+++ likelihood/test_likelihoods/rosenbrock_likelihood.py
@@ -1,47 +1,59 @@
+"""
+rosenbrock_likelihood.py: a test problem for our samplers.
+We sample a Rosenbrock function.
+"""
+
 import os
-import pydesglue
+import sys
 import numpy as np
-try:
-	import pylab
-except ImportError:
-	pylab=None
+from cosmosis.datablock import names as section_names
+from cosmosis.datablock import option_section
+
+
+cosmo = section_names.cosmological_parameters
+likes = section_names.likelihoods
 
 
 class Rosenbrock(object):
-	def __init__(self):
-	        self.a1 = 100.0
-        	self.a2 = 20.0
 
-	def logl(self,p):
-		return -(self.a1 * (p[1] - p[0] ** 2) ** 2 + (1 - p[0]) ** 2) / self.a2
+    def __init__(self):
+        self.a1 = 100.0
+        self.a2 = 20.0
 
-	def __call__(self, p):
-		return logl(self,p)
-		
+    def logl(self, p):
+        return -(self.a1 * (p[1] - p[0] ** 2) ** 2 + (1 - p[0]) ** 2) / self.a2
+
+    def __call__(self, p):
+        return np.log(self, p)
 
 
-def execute(handle):
-	try:
-		package = pydesglue.DesDataPackage.from_fits_handle(handle)
-		section = pydesglue.section_names.cosmological_parameters
-		p=[]
-		p.append(package.get_param(section,"P1"))
-		p.append(package.get_param(section,"P2"))
+def setup(options):
+    return 1
 
-		rosenbrock = Rosenbrock()
-		#compute likelihood
-		like =rosenbrock.logl(p)
-	
-		section = pydesglue.section_names.likelihoods
-		package.set_param(section,"ROSENBROCK_LIKE",like)
-		package.write_to_fits_handle(handle)
-	
-	except KeyboardInterrupt:
-		#If the user presses Ctrl-C we respect that
-		raise KeyboardInterrupt
-	except Exception as E:
-		#But any other kind of error is a problem
-                print "There was an error calculating the Rosenbrock likelihood: "
-                return 1
-        return 0
 
+def execute(block, config):
+    p = []
+    p.append(block[cosmo, "P1"])
+    p.append(block[cosmo, "P2"])
+
+    rosenbrock = Rosenbrock()
+    # compute likelihood
+    like = rosenbrock.logl(p)
+    block[likes, 'ROSENBROCK_LIKE'] = like
+
+    # except KeyboardInterrupt :
+    # If the user presses Ctrl-C we respect that
+    #     raise
+
+    # except Exception as E:
+    # But any other kind of error is a problem
+    #     print "There was an error calculating the Rosenbrock likelihood: "
+    #     return 1
+
+    return 0
+
+
+def cleanup(config):
+    # nothing to do here!  We just include this
+    # for completeness
+    return 0
