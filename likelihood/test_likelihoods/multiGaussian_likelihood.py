d97395125a79f1bbb9bbd896877d3e52403acd47
Alessandro Manzotti <manzotti.alessandro@gmail.com>
2014-05-09 11:09:37 -0500

    fix multigaussian likelihood

--- likelihood/test_likelihoods/multiGaussian_likelihood.py
+++ likelihood/test_likelihoods/multiGaussian_likelihood.py
@@ -3,6 +3,7 @@ MultiGaussian_likelihood.py: a test problem for our samplers.
 We sample a 10 dimensional gaussian.
 """
 import os
+import sys
 import numpy as np
 import os.path
 from cosmosis.datablock import names as section_names
@@ -15,25 +16,26 @@ likes = section_names.likelihoods
 
 class Multigaussian(object):
 
-    def __init__(self, ndim):
+    def __init__(self, ndim, means=None, covariances=None):
         self.ndim = ndim
-        if os.path.isfile('./means10D.out'):
-            self.means = np.loadtxt('./means10D.out')
+        if means:
+            self.means = np.loadtxt(means)
+            # print self.means
             # print "loading saved means"
-        # else:
-        #     self.means = np.random.rand(ndim)
-        #     np.savetxt('./means10D.out', self.means)
+        else:
+            self.means = np.random.rand(ndim)
+            np.savetxt('./demos/means10D.out', self.means)
 
-        if os.path.isfile('./covariance10D.out'):
-            cov = np.loadtxt('./covariance10D.out')
+        if covariances:
+            cov = np.loadtxt(covariances)
             # print "loading saved covariance matrix"
 
-        # else:
-        #     cov = 0.5-np.random.rand(ndim**2).reshape((ndim, ndim))
-        #     cov = np.triu(cov)
-        #     cov += cov.T - np.diag(cov.diagonal())
-        #     cov = np.dot(cov,cov)
-        #     np.savetxt('covariance10D.out', cov)
+        else:
+            cov = 0.5 - np.random.rand(ndim ** 2).reshape((ndim, ndim))
+            cov = np.triu(cov)
+            cov += cov.T - np.diag(cov.diagonal())
+            cov = np.dot(cov, cov)
+            np.savetxt('./demos/covariance10D.out', cov)
 
         self.icov = np.linalg.inv(cov)
         self.det = np.linalg.det(cov)
@@ -50,36 +52,51 @@ class Multigaussian(object):
 
 
 def setup(options):
-    section = option_section
-    return 1
+
+    try:
+        means = options[option_section, "means_filename"]
+        print "Existing means (%s) will be used" % means
+    except Exception, e:
+        means = None
+        print "new means will be generated"
+
+    try:
+        covariances = options[option_section, "cov_filename"]
+        print "Existing covariances (%s) will be used" % covariances
+    except Exception, e:
+        covariances = None
+        print "new covariance matrix will be generated"
+
+    return means, covariances
 
 
 def execute(block, config):
-        x = []
-        x.append(block[cosmo, "MG1"])
-        x.append(block[cosmo, "MG1"])
-        x.append(block[cosmo, "MG1"])
-        x.append(block[cosmo, "MG1"])
-        x.append(block[cosmo, "MG1"])
-        x.append(block[cosmo, "MG1"])
-        x.append(block[cosmo, "MG1"])
-        x.append(block[cosmo, "MG1"])
-        x.append(block[cosmo, "MG1"])
-        x.append(block[cosmo, "MG1"])
-        mg = Multigaussian(len(x))
-        # compute likelihood
-        like = mg.lnprob(x)
-        block[likes, "MULTIGAUSSIAN_LIKE"] = like
+    x = []
+    x.append(block[cosmo, "MG1"])
+    x.append(block[cosmo, "MG2"])
+    x.append(block[cosmo, "MG3"])
+    x.append(block[cosmo, "MG4"])
+    x.append(block[cosmo, "MG5"])
+    x.append(block[cosmo, "MG6"])
+    x.append(block[cosmo, "MG7"])
+    x.append(block[cosmo, "MG8"])
+    x.append(block[cosmo, "MG9"])
+    x.append(block[cosmo, "MG10"])
+    mg = Multigaussian(len(x), config[0], config[1])
+    # compute likelihood
+    like = mg.lnprob(x)
+    block[likes, "MULTIGAUSSIAN_LIKE"] = like
 
-    except KeyboardInterrupt:
-        # If the user presses Ctrl-C we respect that
-        raise KeyboardInterrupt
-    except Exception as E:
-        # But any other kind of error is a problem
-        print "There was an error calculating the MultiGaussian likelihood: "
-        return 1
+    # except KeyboardInterrupt:
+    # If the user presses Ctrl-C we respect that
+    #     raise KeyboardInterrupt
+    # except Exception as E:
+    # But any other kind of error is a problem
+    #     print "There was an error calculating the MultiGaussian likelihood: "
+    #     return 1
 
-        return 0
+    return 0
+
 
 def cleanup(config):
 # nothing to do here!  We just include this
