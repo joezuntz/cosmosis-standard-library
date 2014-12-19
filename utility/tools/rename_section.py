5dec50db2993ec7ac2e8265259d3e82a9b8e4336
Joe Zuntz <joezuntz@googlemail.com>
2014-12-19 15:27:27 +0000

    let the rename section utility rename multiple sections at once

--- utility/tools/rename_section.py
+++ utility/tools/rename_section.py
@@ -2,13 +2,16 @@ from cosmosis.datablock import option_section
 
 def setup(options):
 	source = str(options[option_section, "source"])
+	source = source.split(',')
 	dest = str(options[option_section, "dest"])
+	dest = dest.split(',')
 	return (source, dest)
 
 def execute(block, config):
 	source, dest = config
-	block._copy_section(source, dest)
-	block._delete_section(source)
+	for (s,d) in zip(source,dest):
+		block._copy_section(s, d)
+		block._delete_section(s)
 	return 0
 
 def cleanup(config):
