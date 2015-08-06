4846a51e32810bcc1a4e681596ef323295f3fe87
Simon Samuroff <sws@fillyjonk.localdomain>
2015-08-06 17:47:22 +0100

    With option for bias/power spectrum output

--- intrinsic_alignments/la_model/linear_alignments_interface.py
+++ intrinsic_alignments/la_model/linear_alignments_interface.py
@@ -6,11 +6,12 @@ import numpy as np
 
 def setup(options):
 	method = options[option_section, "method"].lower()
+	bias_option = options[options_section, "bias"]
 	if method not in ["krhb", "bk", "bk_corrected"]:
 		raise ValueError('The method in the linear alignment module must'
 			'be either "KRHB" (for Kirk, Rassat, Host, Bridle) or BK for '
 			'Bridle & King or "BK_corrected" for the corrected version of that')
-	return method
+	return method, bias
 
 def execute(block, config):
 	# load z_lin, k_lin, P_lin, z_nl, k_nl, P_nl, C1, omega_m, H0
@@ -19,7 +20,7 @@ def execute(block, config):
 	ia = names.intrinsic_alignment_parameters
 	cosmo = names.cosmological_parameters
 
-	method = config
+	method, bias_option = config
 
 	z_lin,k_lin,p_lin=block.get_grid(lin,"z","k_h","p_k")
 	z_nl,k_nl,p_nl=block.get_grid(nl,"z","k_h","p_k")
@@ -36,17 +37,28 @@ def execute(block, config):
 	#run computation and write to datablock
 	if method=='krhb':
 		P_II, P_GI, b_I, r_I = kirk_rassat_host_bridle_power(z_lin, k_lin, p_lin, z_nl, k_nl, p_nl, A, omega_m)
-		block.put_grid(ia, "z", z_nl, "k_h", k_nl,  "b_I", b_I)
-		block.put_grid(ia, "z", z_nl, "k_h", k_nl, "r_I", r_I)
+		if bias_option:
+			block.put_grid(ia, "z", z_nl, "k_h", k_nl,  "b_I", b_I)
+			block.put_grid(ia, "z", z_nl, "k_h", k_nl, "r_I", r_I)
+		else:
+			block.put_grid(ia, "z", z_lin, "k_h", k_lin, "P_II", P_II)
+			block.put_grid(ia, "z", z_lin, "k_h", k_lin,  "P_GI", P_GI)
 	elif method=='bk':
 		P_II, P_GI, b_I, r_I = bridle_king(z_nl, k_nl, p_nl, A, omega_m)
-		block.put_grid(ia,  "z", z_nl, "k_h", k_nl, "b_I", b_I)
-		block.put_grid(ia,  "z", z_nl, "k_h", k_nl, "r_I", r_I)
+		if bias_option:
+			block.put_grid(ia,  "z", z_nl, "k_h", k_nl, "b_I", b_I)
+			block.put_grid(ia,  "z", z_nl, "k_h", k_nl, "r_I", r_I)
+		else:
+			block.put_grid(ia, "z", z_nl, "k_h", k_nl, "P_II", P_II)
+			block.put_grid(ia, "z", z_nl, "k_h", k_nl,  "P_GI", P_GI)
 	elif method=='bk_corrected':
 		P_II, P_GI, b_I, r_I = bridle_king_corrected(z_nl, k_nl, p_nl, A, omega_m)
-		block.put_grid(ia, "z", z_nl, "k_h", k_nl, "b_I", b_I)
-		block.put_grid(ia, "z", z_nl, "k_h", k_nl,  "r_I", r_I)
-		
+		if bias_option:
+			block.put_grid(ia, "z", z_nl, "k_h", k_nl, "b_I", b_I)
+			block.put_grid(ia, "z", z_nl, "k_h", k_nl,  "r_I", r_I)
+		else:
+			block.put_grid(ia, "z", z_nl, "k_h", k_nl, "P_II", P_II)
+			block.put_grid(ia, "z", z_nl, "k_h", k_nl,  "P_GI", P_GI)
 	return 0
 
 def cleanup(config):
