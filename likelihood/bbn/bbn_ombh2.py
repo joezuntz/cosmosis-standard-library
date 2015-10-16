2e0f900479cf86e1ce7c6c668e800b3fbfe1ac9a
Joe Zuntz <joezuntz@googlemail.com>
2015-10-16 13:28:25 +0100

    update BBN likelihood to GaussianLikelihood

--- likelihood/bbn/bbn_ombh2.py
+++ likelihood/bbn/bbn_ombh2.py
@@ -1,41 +1,28 @@
-from numpy import log, pi
-from cosmosis.datablock import names as section_names
-from cosmosis.datablock import option_section
+from cosmosis.datablock import names
+from cosmosis.gaussian_likelihood import SingleValueGaussianLikelihood
 
-#The names of sections to read things from
-cosmo = section_names.cosmological_parameters
-likes = section_names.likelihoods
+class BBNLikelihood(SingleValueGaussianLikelihood):
+    # The mean and standard deviation of the BBN measurements.
+    # The user can over-ride these in the ini file if desired
+    mean = 0.023
+    sigma = 0.002
 
+    #The value (either chosen by the sampler or computed
+    #somewhere in the pipeline) that this Likelihood uses.
+    #If we needed to operate on a derived quantity we could
+    #have redefined the method block_theory_points instead
+    section = names.cosmological_parameters
 
-#Particle Data Group 2013
-BBN_OMBH2_MEAN = 0.023
-BBN_OMBH2_SIGMA = 0.002
-
-def setup(options):
-	mean = options.get_double(option_section, "mean", default=BBN_OMBH2_MEAN)
-	sigma = options.get_double(option_section, "sigma", default=BBN_OMBH2_MEAN)
-	norm = 0.5*log(2*pi*sigma**2)
-	return (mean, sigma, norm)
-
-
-def execute(block, config):
-	# Configuration data, read from ini file above
-	mean,sigma,norm = config
+    #Where we should save the likelihood
+    like_name = "bbn"
 
 
-	# Get parameters from sampler
-	h0 = block[cosmo, 'h0']
-	omega_b = block[cosmo, 'omega_b']
+    def extract_theory_points(self, block):
+        omega_b = block[self.section, 'omega_b']
+        h       = block[self.section, 'h0']
+        ombh2 = omega_b * h**2
+        return ombh2
 
-	#compute the likelihood - just a simple Gaussian
-	ombh2 = omega_b * h0**2
-	like = -(ombh2-mean)**2/sigma**2/2.0 - norm
-	block[likes, 'BBN_LIKE'] = like
 
-	#signal that everything went fine
-	return 0
+setup, execute, cleanup = BBNLikelihood.build_module()
 
-def cleanup(config):
-	#nothing to do here!  We just include this 
-	# for completeness
-	return 0
