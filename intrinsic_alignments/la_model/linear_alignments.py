7325604af4e0fc622fd1d5f484d4dc09d8495f70
Niall Maccrann <maccrann@fornax.jb.man.ac.uk>
2016-04-07 15:29:42 +0100

    added linear model

--- intrinsic_alignments/la_model/linear_alignments.py
+++ intrinsic_alignments/la_model/linear_alignments.py
@@ -100,6 +100,36 @@ def bridle_king_corrected(z_nl, k_nl, P_nl, A, Omega_m):
 
 	return P_II, P_GI, b_I, r_I, k_nl
 
+def linear(z_lin, k_lin, P_lin, A, Omega_m):
+	# What was used in CFHTLens and Maccrann et al.
+	#extrapolate our linear power out to high redshift
+	z0 = np.where(z_nl==0)[0][0]
+	nz = len(z_nl)
+
+	ksmall = np.argmin(k_nl)
+	
+	growth = (P_lin[:,ksmall] / P_lin[z0,ksmall])**0.5
+
+	F = - A * C1_RHOCRIT * Omega_m / growth
+
+	# intrinsic-intrinsic term
+	P_II = np.zeros_like(P_lin)
+
+	for i in xrange(nz):
+		P_II[i,:] = F[i]**2 * P_lin[i,:] 
+
+	P_GI = np.zeros_like(P_lin)
+	for i in xrange(nz):
+		P_GI[i] = F[i] * P_lin[i]
+
+
+	# Finally calculate the intrinsic and stochastic bias terms from the power spectra
+	R1 = P_II/P_nl
+	b_I = np.sqrt(R1) * -1.0 * A/abs(A)
+	r_I = P_GI/P_II *b_I
+
+	return P_II, P_GI, b_I, r_I, k_nl
+
 
 def kirk_rassat_host_bridle_power(z_lin, k_lin, P_lin, z_nl, k_nl, P_nl, A, Omega_m):
 	""" 
