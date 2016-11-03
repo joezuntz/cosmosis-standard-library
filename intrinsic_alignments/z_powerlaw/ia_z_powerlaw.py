cac2a8b6c79f3807eee6c804176310243361edb7
Joe Zuntz <joezuntz@googlemail.com>
2016-11-03 14:08:02 +0000

    Update IA z power law

--- intrinsic_alignments/z_powerlaw/ia_z_powerlaw.py
+++ intrinsic_alignments/z_powerlaw/ia_z_powerlaw.py
@@ -1,35 +1,39 @@
-#IA model from maccrann et al. 2015 - NLA with power law redshift scaling
-#P_II(k,z) = ((1+z)^alpha) * F(z))^2 * P_delta(k,z), P_GI(k,z) = (1+z)^alpha * F(z) * P_delta(k,z)
-#The P_II and P_GI before the redshift scaling are already in the block
-#so just read them in and rescale
-
 from cosmosis.datablock import names, option_section
 import numpy as np
 
 def setup(options):
-	return 0
+    name = options.get_string(option_section, "name", default="").lower()
+    if name:
+        suffix = "_" + name
+    else:
+        suffix = ""
+    return {"suffix":suffix}
 
 def execute(block, config):
+    #Get the names of the sections to save to
+    suffix = config['suffix']
+    ia_section = names.intrinsic_alignment_parameters
+    ia_ii = names.intrinsic_power + suffix
+    ia_mi = names.matter_intrinsic_power + suffix
 
-	ia_section = names.intrinsic_alignment_parameters
+    #read in power spectra for P_II and P_MI
+    z,k,p_ii=block.get_grid(ia_ii,"z","k_h","p_k")
+    z,k,p_mi=block.get_grid(ia_mi,"z","k_h","p_k")
 
-	#read in power spectra
-	z,k,p_ii=block.get_grid(ia_section,"z","k_h","P_II")
-	z,k,p_gi=block.get_grid(ia_section,"z","k_h","P_GI")
+    #read alpha from ia_section values section
+    alpha = block[ia_section,'alpha']
+    _,z_grid=np.meshgrid(k,z)
 
-	#read alpha from ia_section values section - JAZ this is a change from the internal one
-	alpha = block[ia_section,'alpha']
-	_,z_grid=np.meshgrid(k,z)
+    #Construct and apply redshift scaling
+    z_scaling=(1+z_grid)**alpha
+    p_ii*=z_scaling**2
+    p_mi*=z_scaling
 
-	#Construct and Apply redshift scaling
-	z_scaling=(1+z_grid)**alpha
-	p_ii*=z_scaling**2
-	p_gi*=z_scaling
+    #Save grid back to the block
+    block.replace_grid(ia_ii, "z", z, "k_h", k, "p_k", p_ii)
+    block.replace_grid(ia_mi, "z", z, "k_h", k, "p_k", p_mi)
 
-	#Save grid
-	block.replace_grid(ia_section, "z", z, "k_h", k, "P_GI", p_gi)
-	block.replace_grid(ia_section, "z", z, "k_h", k, "P_II", p_ii)
-	return 0
+    return 0
 
 def cleanup(config):
-	pass
+    pass
