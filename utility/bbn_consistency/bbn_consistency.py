6597a46be94e5669a61ca210c028510d43d9f91d
Joe Zuntz <joezuntz@googlemail.com>
2018-02-13 16:16:41 +0000

    add feature to use massless_nu in bbn consistency

--- utility/bbn_consistency/bbn_consistency.py
+++ utility/bbn_consistency/bbn_consistency.py
@@ -16,17 +16,27 @@ def setup(options):
     dat = np.genfromtxt(datafile, names=True, comments='#')
     spline = interpolate.bisplrep(dat['ombh2'], dat['DeltaN'], dat['Yp'])
 
+    input_name = options.get_string(
+        option_section, "input_name", default="delta_neff")
+    assert (input_name == "delta_neff") or (input_name == "massless_nu")
+
     # Saved data will be returned later
-    return {'spline': spline, 'ombh2_min': np.min(dat['ombh2']), 'ombh2_max': np.max(dat['ombh2']), 'DeltaN_min': np.min(dat['DeltaN']), 'DeltaN_max': np.max(dat['DeltaN'])}
+    return {'spline': spline, 'ombh2_min': np.min(dat['ombh2']), 'ombh2_max': np.max(dat['ombh2']), 'DeltaN_min': np.min(dat['DeltaN']), 'DeltaN_max': np.max(dat['DeltaN']), 
+        'input_name':input_name}
 
 
 def execute(block, t):
+    input_name = t['input_name']
     # This loads a value from the section "cosmological_parameters" that we read above.
     ombh2 = block[cosmo, "ombh2"]
-    if block.has_value(cosmo, "delta_neff"):
-        DeltaN = block[cosmo, "delta_neff"]
-    else:
-        DeltaN = 0.0
+    if input_name == "delta_neff":
+        if block.has_value(cosmo, "delta_neff"):
+            DeltaN = block[cosmo, "delta_neff"]
+        else:
+            DeltaN = 0.0
+    elif input_name == "massless_nu":
+        DeltaN = block[cosmo, "massless_nu"] + block[cosmo, "massive_nu"] - 3.046
+
 
     # Check for out-of-range parameters
     if ombh2 < t['ombh2_min'] or ombh2 > t['ombh2_max'] or DeltaN < t['DeltaN_min'] or DeltaN > t['DeltaN_max']:
