a4c044016efb7ef85d5f25c75c9b1b4edd9e679c
aguena <michel.aguena@gmail.com>
2018-07-17 18:28:34 -0300

    simplify sigma_cpp more

--- boltzmann/sigma_cpp/sigma.cpp
+++ boltzmann/sigma_cpp/sigma.cpp
@@ -5,14 +5,34 @@
 #include <omp.h>
 #include <gsl/gsl_spline.h>
 #include <gsl/gsl_integration.h>
-#include "InternalVars.h"                                 
 
-double radius(const double M)
+namespace PARAMS
+{
+const long double
+    eps_sigma = 1e-6
+    ;
+const long double
+    ln10 = log(10.0),
+    pi = 3.14159265358979323846264338327950,
+    four_pi_o3 = 4.*pi/3,
+    rho_c = 2.775e11// rho_c/h^2
+    ;
+double
+    kmin,
+    kmax,
+    r
+    ;
+
+gsl_spline * powerspec;
+
+};
+
+double radius(const double M, const double Omega_M)
 {
     return std::pow(M
-            /(PARAMS::MATH::four_pi_o3
-            *PARAMS::PHYS::rho_c
-            *PARAMS::COSM::Omega_M),
+            /(PARAMS::four_pi_o3
+            *PARAMS::rho_c
+            *Omega_M),
             0.333333333333333333333333333333333333333333333333333333333333);	
 }
 //window function     
@@ -24,7 +44,7 @@ double sig_arg(const double lnk,  void * params) {
 
     const double 
         k = exp(lnk),
-        wkR = w(k*PARAMS::DOUB_V::r);
+        wkR = w(k*PARAMS::r);
 
     return gsl_spline_eval(PARAMS::powerspec, k, NULL)
             *wkR*wkR*k*k*k;
@@ -72,7 +92,6 @@ int executemain (
         double* sigma_m         
         ) {
 
-    PARAMS::COSM::Omega_M = OmM; 
     int proc_num = int_config[0];
     int n_k = int_config[1];
     int nm_bin = int_config[2];
@@ -81,15 +100,14 @@ int executemain (
 
     omp_set_num_threads(proc_num);
 
-    PARAMS::DOUB_V::kmin = Pk_k[0];
-    PARAMS::DOUB_V::kmax = Pk_k[n_k-1];
-    printf("kmin=%.10f\tkmax=%.10f\n",PARAMS::DOUB_V::kmin,PARAMS::DOUB_V::kmax);
+    PARAMS::kmin = Pk_k[0];
+    PARAMS::kmax = Pk_k[n_k-1];
+    printf("kmin=%.10f\tkmax=%.10f\n",PARAMS::kmin,PARAMS::kmax);
 
     double tstart, tstop, ttime;
     tstart = (double)clock()/CLOCKS_PER_SEC;
     printf("starting\n");
 
-    //if(PARAMS::INT_V::prt_details==1)printf("Omega_M=%g\n",PARAMS::COSM::Omega_M);
 
     printf("proc_num:%d\n",proc_num);
     printf("n_k:%d\n",n_k);
@@ -100,7 +118,7 @@ int executemain (
     // Writes radius to vector
 
     for (int i=0;i<nm_bin;i++)
-        r_vec[i] = radius(pow(10., m_vec[i]));
+        r_vec[i] = radius(pow(10., m_vec[i]), OmM);
 
     // Converts k vec to std::vector
 
@@ -135,9 +153,9 @@ int executemain (
     double
         romb = 0;
     const double
-        eps = PARAMS::EPS::eps_sigma,
-        lnkmin = std::log(PARAMS::DOUB_V::kmin * 1.001),
-        lnkmax = std::log(PARAMS::DOUB_V::kmax * 0.999);
+        eps = PARAMS::eps_sigma,
+        lnkmin = std::log(PARAMS::kmin * 1.001),
+        lnkmax = std::log(PARAMS::kmax * 0.999);
 
     std::vector<double> ps(n_k);
 
@@ -150,9 +168,9 @@ int executemain (
         PARAMS::powerspec = spline_from_vectors(ps_k, ps);
     
         for (int i=0;i<nm_bin;i++){
-            PARAMS::DOUB_V::r = r_vec[i];
+            PARAMS::r = r_vec[i];
             romb = gsl_integral(sig_arg, lnkmin, lnkmax, eps);
-            sigma_m[i+iz*nm_bin] =  std::sqrt(.5*romb)/PARAMS::MATH::pi;
+            sigma_m[i+iz*nm_bin] =  std::sqrt(.5*romb)/PARAMS::pi;
         }
     }
 
