c8d3dfad0f02faaa16dc06514f96ce42e9b9a22f
Joe Zuntz <joezuntz@googlemail.com>
2019-01-29 09:31:00 +0000

    change growth extractor to use linear matter power by default

--- structure/extract_growth/extract_growth.py
+++ structure/extract_growth/extract_growth.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def setup(options):
     print("Warning: extract_growth is currently being tested.")
-    input_section = options.get_string(option_section, "input_section", names.matter_power_nl)
+    input_section = options.get_string(option_section, "input_section", names.matter_power_lin)
     output_section = options.get_string(option_section, "output_section", names.growth_parameters)
     return [input_section, output_section]
 
