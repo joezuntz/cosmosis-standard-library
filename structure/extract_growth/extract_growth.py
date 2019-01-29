303905a4a9fad5d4292b764e6c2c84e81f2cd582
Joe Zuntz <joezuntz@googlemail.com>
2019-01-29 09:22:13 +0000

    add extract_growth module

---
+++ structure/extract_growth/extract_growth.py
@@ -0,0 +1,35 @@
+from cosmosis.datablock import names, option_section
+import scipy.interpolate
+import numpy as np
+
+def setup(options):
+    print("Warning: extract_growth is currently being tested.")
+    input_section = options.get_string(option_section, "input_section", names.matter_power_nl)
+    output_section = options.get_string(option_section, "output_section", names.growth_parameters)
+    return [input_section, output_section]
+
+def execute(block, config):
+    input_section, output_section = config
+
+    z,k,p = block.get_grid(input_section, "z", "k_h", "p_k")
+
+    k0 = k.argmin()
+    z0 = z.argmin()
+
+    p_z = p[:, k0]
+
+    D = np.sqrt(p_z / p_z[z0])
+    logD = np.log(D)
+
+    a = 1.0/(1.0+z)
+    loga = np.log(a)
+
+    logD_spline = scipy.interpolate.UnivariateSpline(loga[::-1], logD[::-1])
+    f_spline = logD_spline.derivative()
+    f = f_spline(a)
+
+    block[output_section, "z"] = z
+    block[output_section, "d_z"] = D
+    block[output_section, "f_z"] = f
+
+    return 0
