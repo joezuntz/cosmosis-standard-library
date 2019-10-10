e3e7e6eca6946d49226bbb46897641640a0f764b
niallm1 <cond0089@ruby01.osc.edu>
2019-10-10 16:27:21 -0400

    fast pt stuff

--- structure/projection/fastpt_interface.py
+++ structure/projection/fastpt_interface.py
@@ -6,8 +6,10 @@ from scipy.interpolate import InterpolatedUnivariateSpline as intspline
 from scipy.interpolate import RectBivariateSpline
 
 sys.path.insert(0, os.environ['COSMOSIS_SRC_DIR'] + '/cosmosis-des-library/tcp/fast_pt/')
-import FASTPT_2_1.FASTPT as FASTPT
-from FASTPT_2_1.P_extend import k_extend
+#import FASTPT_2_1.FASTPT as FASTPT
+#from FASTPT_2_1.P_extend import k_extend
+import fastpt_develop.FASTPT as FASTPT
+from fastpt_develop.P_extend import k_extend
 import pdb
 
 from cosmosis.datablock import names
