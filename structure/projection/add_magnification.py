bb029cfdc06ab73792ef176e2f5e38bd25b84112
Niall MacCrann <niall.maccrann@gmail.com>
2019-11-27 15:27:36 -0500

    Jack's change adding the mag-intrinsic bit to add_magnification, and also replacing 'position' with 'galaxy' which makes more sense.

--- structure/projection/add_magnification.py
+++ structure/projection/add_magnification.py
@@ -2,7 +2,6 @@
 add the magnification terms to the shear-gal and gal-gal Cls
 based on the add_intrinsic module
 """
-
 from __future__ import print_function
 from builtins import range
 from cosmosis.datablock import option_section, names
@@ -10,36 +9,33 @@ from scipy.interpolate import InterpolatedUnivariateSpline
 import numpy as np
 
 def setup(options):
-    do_position_position = options.get_bool(option_section, "position-position", True)
-    do_position_shear = options.get_bool(option_section, "position-shear", True)
-
+    do_galaxy_galaxy = options.get_bool(option_section, "galaxy-galaxy", True)
+    do_galaxy_shear = options.get_bool(option_section, "galaxy-shear", True)
+    include_intrinsic = options.get_bool(option_section, "include_intrinsic", False)
     print()
     print("The add_magnification module will try to combine magnification terms into")
-    if do_position_position and do_position_shear:
-        print("both the position-position and position-shear spectra.")
-    elif do_position_position:
-        print("only the position_position spectra.")
-    elif do_position_shear:
-        print("only the position-shear.")
+    if do_galaxy_galaxy and do_galaxy_shear:
+        print("both the galaxy-galaxy and galaxy-shear spectra.")
+    elif do_galaxy_galaxy:
+        print("only the galaxy-galaxy spectra.")
+    elif do_galaxy_shear:
+        print("only the galaxy-shear.")
     else:
-        print("... actually not into anything. You set position-position=F and position-shear=F")
+        print("... actually not into anything. You set galaxy-galaxy=F and galaxy-shear=F")
         print("Ths module will not do anything in this configuration")
-
     print()
-    return do_position_position, do_position_shear
+    return do_galaxy_galaxy, do_galaxy_shear, include_intrinsic
 
 def execute(block, config):
-    do_position_position, do_position_shear = config
-
-    if do_position_position:
+    do_galaxy_galaxy, do_galaxy_shear, include_intrinsic = config
+    if do_galaxy_galaxy:
         nbin_pos = block[names.galaxy_cl, 'nbin']
-    elif do_position_shear:
+    elif do_galaxy_shear:
         nbin_pos = block["galaxy_shear_cl", 'nbin_a']
-    if do_position_shear:
+    if do_galaxy_shear:
         nbin_shear = block["galaxy_shear_cl", 'nbin_b']
-
-    if do_position_position:
-        # for position_position, we're replacing 'galaxy_cl' (the gg term) with gg+gm+mg+mm
+    if do_galaxy_galaxy:
+        # for galaxy_galaxy, we're replacing 'galaxy_cl' (the gg term) with gg+gm+mg+mm
         # so in case useful, save the gg term to galaxy_cl_gg.
         ells = block[names.galaxy_cl, 'ell']
         block["galaxy_cl_gg", 'ell'] = ells
@@ -55,20 +51,16 @@ def execute(block, config):
             resample_mag_mag = True
         elif not np.allclose(mag_mag_ells, ells):
             resample_mag_mag = True
-
         #Get auto_only - if True, only auto-correlations for galaxy_cl
         #were computed.
         auto_only = block.get_bool(names.galaxy_cl, "auto_only", False)
-
         for i in range(nbin_pos):
             for j in range(i + 1):
                 if (i!=j and auto_only):
                     continue
-
                 bin_ij = 'bin_{0}_{1}'.format(i + 1, j + 1)
                 bin_ji = 'bin_{1}_{0}'.format(i + 1, j + 1)
                 block["galaxy_cl_gg", bin_ij] = block[names.galaxy_cl, bin_ij]
-
                 gal_mag_ij_orig = block["galaxy_magnification_cl", bin_ij]
                 gal_mag_ji_orig = block["galaxy_magnification_cl", bin_ji]
                 mag_mag_orig = block["magnification_cl", bin_ij]
@@ -80,28 +72,30 @@ def execute(block, config):
                         gal_mag_ij_orig)(np.log(ells[ells>0]))
                     gal_mag_ji[ells>0] = InterpolatedUnivariateSpline(np.log(gal_mag_ells), 
                         gal_mag_ji_orig)(np.log(ells[ells>0]))  
-   
                 else:
                     gal_mag_ij = gal_mag_ij_orig
                     gal_mag_ji = gal_mag_ji_orig
-
                 if resample_mag_mag:
                     mag_mag[ells>0] = InterpolatedUnivariateSpline(np.log(gal_mag_ells), 
                         gal_mag_ji_orig)(np.log(ells[ells>0]))   
                 else:
                     mag_mag = mag_mag_orig    
-
                 block[names.galaxy_cl, bin_ij] += (
                     gal_mag_ij + gal_mag_ji
                     + mag_mag   #mm
                 )
-                
-    if do_position_shear:
+    #if include_intrinsic is True, we're replacing gG+gI with gG+gI+mG+mI       
+    #else,                         we're replacing gG    with gG+mG 
+    if do_galaxy_shear:
         for i in range(nbin_pos):
             for j in range(nbin_shear):
                 bin_ij = 'bin_{0}_{1}'.format(i + 1, j + 1)
                 block["galaxy_shear_cl_gG", bin_ij] = block["galaxy_shear_cl", bin_ij]
                 block["galaxy_shear_cl", bin_ij] += (
                     block["magnification_shear_cl", bin_ij]
-                    )                    
+                    )
+                if include_intrinsic == True:
+                    block["galaxy_shear_cl", bin_ij] += (
+                        block["magnification_intrinsic_cl", bin_ij]
+                        )
     return 0
