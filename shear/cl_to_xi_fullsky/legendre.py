shear bin average fixes

shear/cl_to_xi_fullsky/legendre.py
@@ -329,26 +329,55 @@ def get_legfactors_00_binav(ells, theta_edges):
     #print('legfacs = ',legfacs)
     return legfacs
 
-def get_legfactors_22_binav(ells, thetas):
-    print('getting bin averaged leg factors')
-    n_ell, n_theta = len(ells), len(thetas)
-    theta_edges = theta_bin_means_to_edges(thetas) # this does geometric mean
-    #theta_edges = [0.00072722, 0.00091552, 0.00115257, 0.001451  , 0.0018267 ,
-    #       0.00229967, 0.00289512, 0.00364474, 0.00458845, 0.00577652,
-    #       0.00727221, 0.00915516, 0.01152567, 0.01450996, 0.01826695,
-    #       0.02299673, 0.02895117, 0.03644736, 0.04588451, 0.05776518,
-    #       0.07272205] #these are hard-coded values for the standard cosmolike comparison
-    leg_factors_p = np.zeros((n_theta, n_ell))
-    leg_factors_m = np.zeros((n_theta, n_ell))
-    ell_factor = np.zeros(len(ells))
-    ell_factor[2:] = (2 * ells[2:] + 1) / 2. / PI / ells[2:] / ells[2:] / (ells[2:]+1) / (ells[2:]+1)
-    for it, t in enumerate(theta_edges[1:]):
-        t_min = theta_edges[it]
-        t_max = t
-        cost_min = np.cos(t_min) # thetas are already converted to radians
-        cost_max = np.cos(t_max)
-        gp, gm = Gp_plus_minus_Gm_binav(ells, cost_min, cost_max)
-        leg_factors_p[it] = ell_factor * gp
-        leg_factors_m[it] = ell_factor * gm
+def Gp_plus_minus_Gm_binav(ells, cost_min, cost_max):
+    """Calculate bin-averaged G_{l,2}^{+/-}"""
+    Gp_plus_Gm  = np.zeros(len(ells))
+    Gp_minus_Gm = np.zeros(len(ells))
 
-    return ( leg_factors_p, leg_factors_m )
+    # for ell=0,1 it is 0
+    Gp_plus_Gm[0:1]  = 0.
+    Gp_minus_Gm[0:1] = 0.
+
+    # for the rest of ell's compute equation (5.8) in https://arxiv.org/abs/1911.11947
+    ell = ells[2:]
+    #---coefficients including only P_l
+    coeff_lm1     = -ell*(ell-1.)/2. * (ell+2./(2.*ell+1)) - (ell+2.)
+    coeff_lp1     = ell*(ell-1.)/(2.*ell+1.)
+    coeff_l       = -ell*(ell-1.)*(2.-ell)/2.
+    coeff_l_plus  = coeff_l - 2.*(ell-1.)
+    coeff_l_minus = coeff_l + 2.*(ell-1.)
+    #---coefficients including dP_l/dx
+    coeff_dlm1_plus   = -2.*(ell+2.)
+    coeff_xdl_plus    = 2.*(ell-1.)
+    coeff_xdlm1       = ell+2.
+    coeff_dl          = 4.-ell
+
+    # computation of legendre polynomials
+    #---this computes all polynomials of order 0 to ell_max+1 and for all ell's
+    lpns_min  = lpn(ell[-1]+1, cost_min)[0][1:]
+    lpns_max  = lpn(ell[-1]+1, cost_max)[0][1:]
+    dlpns_min = lpn(ell[-1]+1, cost_min)[1][1:]
+    dlpns_max = lpn(ell[-1]+1, cost_max)[1][1:]
+
+    # denominator in average
+    dcost = cost_max-cost_min
+
+    # numerator in average
+    #---common part in both plus and minus
+    common_part  = coeff_lm1*(lpns_max[:-2]-lpns_min[:-2])
+    common_part += coeff_l*(cost_max*lpns_max[1:-1] - cost_min*lpns_min[1:-1])
+    common_part += coeff_lp1*(lpns_max[2:]-lpns_min[2:])
+    common_part += coeff_dl*(dlpns_max[1:-1]-dlpns_min[1:-1])
+    common_part += coeff_xdlm1*(cost_max*dlpns_max[:-2]-cost_min*dlpns_min[:-2])
+    #---plus
+    Gp_plus_Gm_extra  = coeff_xdl_plus*(cost_max*dlpns_max[1:-1]-cost_min*dlpns_min[1:-1])
+    Gp_plus_Gm_extra += -coeff_xdl_plus*(lpns_max[1:-1] - lpns_min[1:-1])
+    Gp_plus_Gm_extra += coeff_dlm1_plus*(dlpns_max[:-2]-dlpns_min[:-2])
+    Gp_plus_Gm[2:] = common_part + Gp_plus_Gm_extra
+    Gp_plus_Gm /= dcost
+    #---minus
+    Gp_minus_Gm_extra = -Gp_plus_Gm_extra
+    Gp_minus_Gm[2:] = common_part + Gp_minus_Gm_extra
+    Gp_minus_Gm /= dcost
+
+    return Gp_plus_Gm, Gp_minus_Gm