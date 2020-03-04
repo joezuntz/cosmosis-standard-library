69066b513f3d4a7a89991f75a6a3bab9b590880e
Joe Zuntz <joezuntz@googlemail.com>
2020-03-04 19:32:59 +0000

    add lots of comments

--- structure/baryoniceffects/baryonic.py
+++ structure/baryoniceffects/baryonic.py
@@ -15,52 +15,63 @@ from builtins import range
 from builtins import object
 import numpy as np
 import re
-try:
-    import scipy.interpolate
-except ImportError:
-    sys.stderr.write(
-        "The eagle baryon power code requires the scipy python module\n")
-    sys.stderr.write("but it could not be found.  This code will fail.\n")
-    raise ImportError(
-        "Require scipy module to run baryon code.  Could not find it.")
+import scipy.interpolate
 
 import os
 dirname = os.path.split(__file__)[0]
 
 # File names
         
-class RatioDatPowerModulator():
+class RatioDatPowerModulator(object):
     """ using hunjing's logpk ratio fits file. redshift list is specific to her file"""
 
-    def __init__(self, ratiofile):
-        #print(ratiofile)
-        f = open(ratiofile, 'r')
+    def __init__(self, ratio_file):
+        # Load the first line of the ratio file, which lists the
+        # column names, each of which describes a different z value
+        f = open(ratio_file, 'r')
         line = f.readline()
         f.close()
-        colname_list = line.replace('\n','').split(' ')[1:]
-        # assuming read-in redshift list is in decending order
-        colname_list.reverse()
-        #print(colname_list)
-        redshift_list = np.zeros(len(colname_list))
-        for i,col in enumerate(colname_list):
+        colnames = line.replace('\n','').split(' ')[1:]
+
+        # Assuming read-in redshift list is in decending order
+        colnames.reverse()
+
+        # Parse the column names to determine the redshifts they refer to.
+        # They are in the form "logPk197" -> z = 1.97
+        nz = len(colnames)
+        redshifts = np.zeros(nz)
+        for i,col in enumerate(colnames):
+            # Pull out the integer part
             zintstr = re.findall('\d+',col)[0]
-            redshift_list[i]  = int(zintstr)/10.0**(len(zintstr)-1)
-        #print("hydro sim redshifts:",redshift_list)
-        data = np.loadtxt(ratiofile,skiprows=1)
+            # assume decimal place is after first digit
+            redshifts[i]  = int(zintstr)/10.0**(len(zintstr)-1)
+
+        print("Reading baryonic effects table {}, which has {} redshift values in".format(ratio_file, nz))
+
+        data = np.loadtxt(ratio_file,skiprows=1)
         logk = data[:,0]
-        #print((len(logk),len(redshift_list)))
-        logpkratio = np.zeros((len(logk),len(redshift_list)))
-        for i in range(len(redshift_list)):
+
+        logpkratio = np.zeros((len(logk),nz))
+
+        # Again, we need to reverse the ordering of the file to put it
+        # in redshift order
+        for i in range(nz):
             logpkratio[:,i] = data[:,-(i+1)]
-        self.ratio = scipy.interpolate.RectBivariateSpline(logk, redshift_list, logpkratio)
+
+        # Make the spline that scales everything
+        self.ratio = scipy.interpolate.RectBivariateSpline(logk, redshifts, logpkratio)
         self.ratio_k = logk
 
     def modulate(self, k, z, P, r=None):
+        # They are definitely logs to base 10, because if you look
+        # at the file 10**log_k_max is a nice round number
         logk = np.log10(k)
+
+        # Compute the DM -> scenario scaling factor
         s = self.ratio(logk,z)
-        #s[k < self.ratio_k[0]] = 0.0
         modulation = 10**s
-        print(modulation.shape,P.shape)
+
+        # and scale and return the power
         P_out = P*modulation
         return P_out
 
--- structure/baryoniceffects/baryonic_interface.py
+++ structure/baryoniceffects/baryonic_interface.py
@@ -3,65 +3,40 @@ An interface to the baryonic code.
 
 """
 from __future__ import print_function
-from builtins import range
 from cosmosis.datablock import option_section, names as section_names
 import baryonic
 import numpy as np
 import os
 
-baryonPowerModulator = None
-ALREADY_DONE_KEY = "BARYONICEFFECTSDONE"
 
 
 def setup(options):
     section = option_section
-    mode = options.get_string(option_section, "mode", "ratio")
 
     try:
-        ratiotable = options[option_section, 'ratiotable']
+        ratio_table = options[option_section, 'ratio_table']
     except KeyError:
-        raise ValueError(
-            "Please set the parameter powtable in the owls section of the ini file, pointing to a ratio file to use")
-        #print("Selected ratio-mode baryons.  Loading table from ", ratiotable)
+        raise ValueError("Please set the parameter powtable in the owls section "
+            "of the ini file, pointing to a ratio file to use")
 
-    baryonPowerModulator = baryonic.RatioDatPowerModulator(ratiotable)
+    # This objejct does the actual work
+    baryonPowerModulator = baryonic.RatioDatPowerModulator(ratio_table)
 
     return baryonPowerModulator
 
 
-"""
-def load_parameters(package, modulator):
-	section = pydesglue.section_names.baryon_parameters
-	if isinstance(modulator, owls.ChebyshevBaryonPowerModulator):
-		rk = [package.get_param(section, "OWLS_K%d"%i) for i in xrange(1,modulator.nterm+1)]
-		rz = [package.get_param(section, "OWLS_Z%d"%i) for i in xrange(1,modulator.nterm+1)]
-		r = np.concatenate([rk, rz])
-	elif isinstance(modulator, owls.FixedBaryonPowerModulator):
-		r=None
-	elif isinstance(modulator, owls.BaryonPowerModulator):
-		r = package.get_param(section, "OWLS_R")
-	elif isinstance(modulator, owls.ScaledBaryonPowerModulator):
-		r = package.get_param(section, "OWLS_R")	
-	else:
-		raise ValueError("Internal error - modulator type not understood in OWLS")
-	return r
-"""
-
-
-def load_parameters(block, modulator):
-    r=None
-    return r
-
-
 def execute(block, config):
-    # JAZ some day we will switch to get this from an input,
-    # so that the modulator only happens once
     modulator = config
-    r = load_parameters(block, modulator)
+
+    # Load the current unmodulated matter power spectrum
     section = section_names.matter_power_nl
-    # load other current values
     k, z, P = (block.get_grid(section, "k_h", "z", "P_k"))
-    print(P.shape)
-    P_mod = modulator.modulate(k, z, P, r)
+    
+    # Apply the scaling as a function of k and z
+    P_mod = modulator.modulate(k, z, P)
+
+    # Update the stored values
     block.replace_grid(section, "k_h", k, "z", z, "P_k", P_mod)
+
+    # All is good - return
     return 0
