852dcb7b08339385cda72628f73e2f12f9733e75
Marc Paterno <paterno@fnal.gov>
2020-09-18 15:42:33 -0500

    Correct name of block into which parameters are injected

--- supernovae/jla_v3/test_jla.c
+++ supernovae/jla_v3/test_jla.c
@@ -3,6 +3,7 @@ void * setup(void * options);
 #include <dlfcn.h>
 #include <assert.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "cosmosis/datablock/c_datablock.h"
 
@@ -19,15 +20,15 @@ int main(void) {
         /* Create a datablock with the right entries */
         c_datablock* config = make_c_datablock();
         assert(config);
-        c_datablock_put_double(config, "jla", "scriptmcut", 10.0);
-        c_datablock_put_string(config, "jla", "data_dir", "data");
-        c_datablock_put_string(config, "jla", "data_file", "jla_lcparams.txt");
-        c_datablock_put_string(config, "jla", "mag_covmat_file", "jla_v0_covmatrix.dat");
-        c_datablock_put_string(config, "jla", "stretch_covmat_file", "jla_va_covmatrix.dat");
-        c_datablock_put_string(config, "jla", "colour_covmat_file", "jla_vb_covmatrix.dat");
-        c_datablock_put_string(config, "jla", "mag_stretch_covmat_file", "jla_v0a_covmatrix.dat");
-        c_datablock_put_string(config, "jla", "mag_colour_covmat_file", "jla_v0b_covmatrix.dat");
-        c_datablock_put_string(config, "jla", "stretch_colour_covmat_file", "jla_vab_covmatrix.dat");                                
+        c_datablock_put_double(config, "module_options", "scriptmcut", 10.0);
+        c_datablock_put_string(config, "module_options", "data_dir", "data");
+        c_datablock_put_string(config, "module_options", "data_file", "jla_lcparams.txt");
+        c_datablock_put_string(config, "module_options", "mag_covmat_file", "jla_v0_covmatrix.dat");
+        c_datablock_put_string(config, "module_options", "stretch_covmat_file", "jla_va_covmatrix.dat");
+        c_datablock_put_string(config, "module_options", "colour_covmat_file", "jla_vb_covmatrix.dat");
+        c_datablock_put_string(config, "module_options", "mag_stretch_covmat_file", "jla_v0a_covmatrix.dat");
+        c_datablock_put_string(config, "module_options", "mag_colour_covmat_file", "jla_v0b_covmatrix.dat");
+        c_datablock_put_string(config, "module_options", "stretch_colour_covmat_file", "jla_vab_covmatrix.dat");
 
         /* Test that our function behaves correctly */
         void* calculator = (psetup)(config);
