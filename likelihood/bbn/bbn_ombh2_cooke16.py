34c261694792d22e3efb52b0d6ab0702bbbec560
Youngsoo Park <youngsoo.park@ipmu.jp>
2021-03-22 14:51:18 -0700

    Adding the Y1 (cooke16) and the new (pitrou20_cooke18) BBN likelihoods

---
+++ likelihood/bbn/bbn_ombh2_cooke16.py
@@ -0,0 +1,30 @@
+# Based on https://arxiv.org/abs/1607.03900
+
+from cosmosis.datablock import names
+from cosmosis.gaussian_likelihood import SingleValueGaussianLikelihood
+
+
+class BBNLikelihood(SingleValueGaussianLikelihood):
+    # The mean and standard deviation of the BBN measurements.
+    # The user can over-ride these in the ini file if desired
+
+    mean = 0.02208
+    sigma = 0.00052
+
+    # The value (either chosen by the sampler or computed
+    # somewhere in the pipeline) that this Likelihood uses.
+    # If we needed to operate on a derived quantity we could
+    # have redefined the method block_theory_points instead
+    section = names.cosmological_parameters
+
+    # Where we should save the likelihood
+    like_name = "bbn"
+
+    def extract_theory_points(self, block):
+        omega_b = block[self.section, 'omega_b']
+        h = block[self.section, 'h0']
+        ombh2 = omega_b * h**2
+        return ombh2
+
+
+setup, execute, cleanup = BBNLikelihood.build_module()
---
+++ likelihood/bbn/bbn_ombh2_pitrou20_cooke18.py
@@ -0,0 +1,41 @@
+# Based on https://arxiv.org/abs/2011.11320 and https://arxiv.org/abs/1710.11129
+
+from cosmosis.datablock import names
+from cosmosis.gaussian_likelihood import SingleValueGaussianLikelihood
+
+
+class BBNLikelihood(SingleValueGaussianLikelihood):
+    # The mean and standard deviation of the BBN measurements.
+    # The user can over-ride these in the ini file if desired
+
+    luna_mn = 0.02195
+    luna_sd = 0.00022
+
+    cooke18_theory_mn = 0.02166
+    cooke18_theory_sd = 0.00019
+
+    invcov_weighted_mn = (luna_mn/luna_sd**2 + cooke18_theory_mn/cooke18_theory_sd**2) / (1/luna_sd**2 + 1/cooke18_theory_sd**2)
+
+    sd_stat = luna_sd
+    sd_sys = ombh_mn - invcov_weighted_mn
+
+    mean = luna_mn
+    sigma = np.sqrt(sd_stat**2 + sd_sys**2)
+
+    # The value (either chosen by the sampler or computed
+    # somewhere in the pipeline) that this Likelihood uses.
+    # If we needed to operate on a derived quantity we could
+    # have redefined the method block_theory_points instead
+    section = names.cosmological_parameters
+
+    # Where we should save the likelihood
+    like_name = "bbn"
+
+    def extract_theory_points(self, block):
+        omega_b = block[self.section, 'omega_b']
+        h = block[self.section, 'h0']
+        ombh2 = omega_b * h**2
+        return ombh2
+
+
+setup, execute, cleanup = BBNLikelihood.build_module()
