07512fce45b46c9175ca8841d85915c4ff5f111d
Joe Zuntz <joezuntz@googlemail.com>
2022-07-04 14:27:15 +0100

    Add WMAP downloader and update Makefile to use corresponding path

---
+++ examples/get-wmap-data.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+pushd likelihood/wmap9/data
+
+echo Downloading WMAP data. This usually takes about 10 minutes.
+echo The bandwidth from the NASA archive is the limiting factor.
+
+wget https://lambda.gsfc.nasa.gov/data/map/dr5/dcp/likelihood/wmap_likelihood_full_v5.tar.gz
+tar -zxvf wmap_likelihood_full_v5.tar.gz
+
+# remove the likelihood code itself as we already have a copy
+mv wmap_likelihood_v5/data/* .
+
+# clean up
+rm -rf wmap_likelihood_v5 wmap_likelihood_full_v5.tar.gz
+popd
