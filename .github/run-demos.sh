83c74f2d36178996f9bd8f4106548562e044423a
Joe Zuntz <joezuntz@googlemail.com>
2022-11-16 15:54:50 +0000

    add homebrew test

--- .github/run-demos.sh
+++ .github/run-demos.sh
@@ -2,10 +2,53 @@
 
 set -e
 
-# Bash is insane. If you don't do this next bit then it
-# decides that you wanted to pass all the arguments from this
-# script on to cosmosis-configure. Madness.
-function DoSource() { source cosmosis-configure ; } ; DoSource
+mkdir output
+
+# Check the downloader works
+./examples/get-planck-data.sh
+
+
+# Run some examples
+cosmosis examples/planck.ini | tee output/planck.log
+grep 'Likelihood =  -6288.61' output/planck.log
+
+
+cosmosis examples/planck_class.ini | tee output/class.log
+# This may need updating as we modify the class interface
+# The settings are not optimized
+# grep 'Likelihood =  -5968.93' output/class.log
+# This seems to give different results on different systems
+
+
+cosmosis examples/wmap.ini | tee output/wmap.log
+
+cosmosis examples/pantheon.ini -p emcee.samples=20
+cosmosis-postprocess examples/pantheon.ini -o output/pantheon
+test -f output/pantheon/cosmological_parameters--omega_m.png
+
+cosmosis examples/des-y1.ini | tee output/des-y1.log
+grep 'Likelihood =  5237.37' output/des-y1.log
+
+cosmosis examples/des-y1.ini -p 2pt_shear.file=./shear/cl_to_corr/cl_to_corr.py 2pt_shear.corr_type=xi | tee output/des-y1.log
+grep 'Likelihood =  5237.3' output/des-y1.log
+
+cosmosis examples/des-y3.ini -p pk_to_cl_gg.save_kernels=T pk_to_cl.save_kernels=T | tee output/des-y3.log
+grep 'Likelihood =  6043.23' output/des-y3.log
+
+cosmosis examples/des-y3-mira-titan.ini | tee output/des-y3-mt.log
+grep 'Likelihood =  6048.0' output/des-y3-mt.log
+
+cosmosis examples/des-y3.ini -v halo_model_parameters.logT_AGN=8.2 -p camb.halofit_version=mead2020_feedback | tee output/des-y3.log
+grep 'Likelihood =  6049.94' output/des-y3.log
+
+cosmosis examples/euclid-emulator.ini
+          test -f output/euclid-emulator/matter_power_nl/p_k.txt
+
+cosmosis examples/w_model.ini
+
+
+
+
 
 
 # for demo 11 first run
