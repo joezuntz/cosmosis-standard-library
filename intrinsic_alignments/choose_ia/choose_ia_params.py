65fa4f77b7a09ed8c9921851307461f59dd01dd5
Catherine Heymans <heymans@roe.ac.uk>
2023-08-11 10:21:43 +0100

    choose_ia/choose_ia_params.py:
    Very short script that allows for the use of
    independent IA parameters for different cosmic
    shear surveys

---
+++ intrinsic_alignments/choose_ia/choose_ia_params.py
@@ -0,0 +1,15 @@
+from cosmosis.datablock import option_section, names
+
+def setup(options):
+    suffix = options.get_string(option_section, "suffix", "")
+    return suffix
+
+def execute(block, config):
+    suffix = config
+
+    for sec,name in block.keys():
+        if (sec=='intrinsic_alignment_parameters') & (suffix in name):
+            out_name = name.replace('_%s'%suffix,'')
+            block[sec,out_name] = block[sec,name]
+
+    return 0
