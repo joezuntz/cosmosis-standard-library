40ba8fe159acec31b78500d98cf1586f44c7f81a
Joe Zuntz <joezuntz@googlemail.com>
2023-08-17 15:07:36 +0100

    switch to theta_h0 from camb

--- utility/consistency/theta_h0.py
+++ utility/consistency/theta_h0.py
@@ -1,32 +1,21 @@
-from astropy.cosmology import LambdaCDM
-from astropy import units, constants
+import camb
 import numpy as np
 import scipy.integrate
 import scipy.optimize
 
-def dtauda(a, cosmo):
-    z = 1.0 / a - 1
-    H = cosmo.H(z)
-    return  (constants.c / (H * a**2)).to("Mpc").value
 
-def dsound_da(a, cosmo):
-    ombh2 = cosmo.Ob0 * cosmo.h**2
-    R = 3.0e4 * a * ombh2
-    c_s = 1.0 / np.sqrt(3*(1+R))
-    return dtauda(a, cosmo) * c_s
-
-def H0_to_theta(hubble, omega_nu, omnuh2, omega_m, ommh2, omega_c, omch2, omega_b, ombh2, omega_lambda, omlamh2, omega_k):
+def H0_to_theta(hubble, omega_nu, omnuh2, omega_m, ommh2, omega_c, omch2, omega_b, ombh2, omega_lambda, omlamh2, omega_k, final=False):
     h = hubble / 100.
     if np.isnan(omnuh2):
         omnuh2 = omega_nu * h**2
     if np.isnan(omega_m):
         omega_m = ommh2 / h**2
-    if np.isnan(omega_b):
-        omega_b = ombh2 / h**2
+    if np.isnan(ombh2):
+        ombh2 = omega_b * h**2
     if np.isnan(omega_lambda):
         omega_lambda = omlamh2 / h**2
-    if np.isnan(omega_c):
-        omega_c = omch2 / h**2
+    if np.isnan(omch2):
+        omch2 = omega_c * h**2
     if np.isnan(omega_m):
         omega_m = omega_c + omega_b + omega_nu
     if np.isnan(omega_m):
@@ -34,20 +23,29 @@ def H0_to_theta(hubble, omega_nu, omnuh2, omega_m, ommh2, omega_c, omch2, omega_
     if np.isnan(omega_lambda):
         omega_lambda = 1 - omega_k - omega_m
 
-    if np.isnan([omnuh2, hubble, omega_m, omega_lambda, omega_b]).any():
+    if np.isnan([omnuh2, hubble, omega_m, omega_lambda, ombh2]).any():
         return np.nan
     
-    m_nu = 93.14 * omnuh2 * units.eV
-    cosmo = LambdaCDM(hubble, omega_m, omega_lambda, m_nu=m_nu/3, Ob0=omega_b, Tcmb0=2.7255, Neff=3.046)
+    mnu = 93.14 * omnuh2
 
-    ombh2 = cosmo.Ob0 * cosmo.h**2
-    omdmh2 = cosmo.Om0 * cosmo.h**2
+    original_feedback_level = camb.config.FeedbackLevel
 
-    zstar = 1048*(1+0.00124*ombh2**(-0.738))*(1+ (0.0783*ombh2**(-0.238)/(1+39.5*ombh2**0.763)) * (omdmh2+ombh2)**(0.560/(1+21.1*ombh2**1.81)))
-    astar = 1 / (1+zstar)
-    rs = scipy.integrate.romberg(dsound_da, 1e-8, astar, rtol=5e-5, args=(cosmo,), divmax=10, vec_func=True) # in Mpc
-    DA = cosmo.angular_diameter_distance(zstar).to("Mpc").value / astar
-    return rs / DA
+    try:
+        camb.set_feedback_level(0)
+        p = camb.CAMBparams()
+        p.set_cosmology(ombh2 = ombh2,
+                        omch2 = omch2,
+                        omk = omega_k,
+                        mnu = mnu,
+                        H0=hubble)
+        r = camb.get_background(p)
+        theta = r.cosmomc_theta()
+    except:
+        theta = np.nan
+    finally:
+        camb.config.FeedbackLevel = original_feedback_level
+    
+    return theta
 
 
 def H0_to_theta_interface(params):
@@ -64,11 +62,10 @@ def H0_to_theta_interface(params):
     omlamh2 = params.get('omlamh2', np.nan)
     omega_k = params.get('omega_k', np.nan)
 
-    return H0_to_theta(hubble, omega_nu, omnuh2, omega_m, ommh2, omega_c, omch2, omega_b, ombh2, omega_lambda, omlamh2, omega_k)
+    return 100 * H0_to_theta(hubble, omega_nu, omnuh2, omega_m, ommh2, omega_c, omch2, omega_b, ombh2, omega_lambda, omlamh2, omega_k)
 
 
 def theta_to_H0(theta, omega_nu, omnuh2, omega_m, ommh2, omega_c, omch2, omega_b, ombh2, omega_lambda, omlamh2, omega_k):
-
     get_theta = lambda H0: H0_to_theta(H0, omega_nu, omnuh2, omega_m, ommh2, omega_c, omch2, omega_b, ombh2, omega_lambda, omlamh2, omega_k)
     t = get_theta(70.0)
 
@@ -79,22 +76,23 @@ def theta_to_H0(theta, omega_nu, omnuh2, omega_m, ommh2, omega_c, omch2, omega_b
     H0_max = 120.0
 
     try:
-        theta_min = get_theta(H0_min)
+        get_theta(H0_min)
     except ValueError:
         H0_min = 40.0
 
     try:
-        theta_max = get_theta(H0_max)
+        get_theta(H0_max)
     except ValueError:
         H0_max = 100.0
 
     f = lambda H0: get_theta(H0) - theta
     H0 = scipy.optimize.brentq(f, H0_min, H0_max, rtol=5e-5)
+
     return H0
 
 
-def theta_to_H0_interface(params):
-    theta = params['cosmomc_theta']
+def theta_to_H0_interface(params):  
+    theta = params['cosmomc_theta'] / 100
     omega_nu = params.get('omega_nu', np.nan)
     omnuh2 = params.get('omnuh2', np.nan)
     omega_m = params.get('omega_m', np.nan)
