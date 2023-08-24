5d4c1adecf92a844f025038d04b72855112ef631
martin-millon <martin.millon@epfl.ch>
2023-08-24 16:23:20 -0700

    second commit

--- likelihood/tdcosmo/tdcosmo_likelihood.py
+++ likelihood/tdcosmo/tdcosmo_likelihood.py
@@ -2,12 +2,20 @@ import os
 import pickle
 from cosmosis.datablock import names, SectionOptions
 from hierarc.Likelihood.lens_sample_likelihood import LensSampleLikelihood
+from astropy.cosmology import LambdaCDM
+from lenstronomy.Cosmo.cosmo_interp import CosmoInterp
+from scipy.interpolate import interp1d
 
 class TDCOSMOlenses:
     def __init__(self, options):
         dir_path = os.path.dirname(__file__)
 
-        #todo: unpack options to select the likelihood. Default should be the 7 TDCOSMO lenses only
+        #todo: unpack options to select the cosmology. Default is LCDM
+        self.cosmo_model = options.get_string("cosmo_model", default='LambdaCDM')
+        if self.cosmo_model == 'LambdaCDM':
+            self.cosmology = LambdaCDM
+        else:
+            raise NotImplementedError()
 
         # 7 TDCOSMO lenses
         file = open(os.path.join(dir_path, 'tdcosmo7_likelihood_processed.pkl'), 'rb')
@@ -39,8 +47,22 @@ class TDCOSMOlenses:
         # TDCOSMO 7 likelihood
         # ====================
 
-        # hear we build a likelihood instance for the sample of 7 TDCOSMO lenses
-        self.tdcosmo7_likelihood = LensSampleLikelihood(tdcosmo7_likelihood_processed)
+        # hear we build a likelihood instance for the sample of 7 TDCOSMO lenses,
+        self.data_sets = options.get_string("data_sets", default='tdcosmo7')
+        lens_list = []
+        if 'tdcosmo7' in self.data_sets:
+            lens_list += tdcosmo7_likelihood_processed
+        if 'SLACS_SDSS' in self.data_sets:
+            lens_list += slacs_sdss_likelihood_processed
+        if 'SLACS_IFU' in self.data_sets:
+            lens_list += slacs_ifu_likelihood_processed
+
+        assert len(lens_list)>0, "Data not found ! Add at least one of those 3 data sets 'tdcosmo7', 'SLACS_SDSS' or 'SLACS_IFU'"
+        #choose which likelihood you want here:
+        self.likelihood = LensSampleLikelihood(lens_list)
+
+        #choose if you want the full astropy distance calculation or a interpolated version of it (for speed-up)
+        self._interpolate_distances_type = 'None'
 
     def cosmosis_cosmo_2_astropy_cosmo(self, block):
         """
@@ -49,14 +71,26 @@ class TDCOSMOlenses:
         :return ~astropy.cosmology equivalent cosmology object
         """
         h = block['cosmological_parameters', 'h0']
-        ok = block['cosmological_parameters', 'omega_k']
+        om = block['cosmological_parameters', 'omega_m']
+        ol = block['cosmological_parameters', 'omega_l'] #todo: check this, not sure how parameters are named
 
-        z_bg = block['distances', 'z']
-        D_A = block['distances', 'D_A']
+        if self._interpolate_distances_type == 'CosmoInterp':
+            cosmo = self.cosmology(H0=h, Om0=om, Ode0=ol)
+            cosmo = CosmoInterp(cosmo=cosmo, z_stop=5, num_interp=100)
+        elif self._interpolate_distances_type == 'None':
+            #we are falling back to astropy cosmology (might be slow)
+            cosmo = self.cosmology(H0=h, Om0=om, Ode0=ol)
+        elif self._interpolate_distances_type == 'cosmosis':
+            #use the distance redshift relation of cosmosis
+            z_bg = block['distances', 'z']
+            D_A = block['distances', 'D_A']
+            angular_distance = interp1d(z_bg, D_A, kind='linear')
+            # todo to be implemented as in the holicow module
+            raise NotImplementedError()
+        else:
+            raise ValueError()
 
-        # TODO: make wrapper to translate cosmosis transverse angular diameter distance to CosmoInterp class that operates under hierArc conventions
-        from lenstronomy.Cosmo.cosmo_interp import CosmoInterp
-        cosmo = CosmoInterp(cosmo=cosmo, z_stop=5, num_interp=100)
+
 
         return cosmo
 
@@ -74,7 +108,7 @@ class TDCOSMOlenses:
                        'a_ani_sigma': 0.3,  # sigma(a_ani)⟨a_ani⟩ is the 1-sigma Gaussian scatter in a_ani
                       }
 
-        logl = self.tdcosmo7_likelihood.log_likelihood(cosmo=cosmo, kwargs_lens=kwargs_lens_test, kwargs_kin=kwargs_kin_test)
+        logl = self.likelihood.log_likelihood(cosmo=cosmo, kwargs_lens=kwargs_lens_test, kwargs_kin=kwargs_kin_test)
 
         return float(logl)
 
