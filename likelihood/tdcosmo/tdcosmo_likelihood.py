174b06a8b1b7d51624fa3628393ed5e07350b173
martin-millon <martin.millon@epfl.ch>
2023-08-25 16:00:43 -0700

    running example

--- likelihood/tdcosmo/tdcosmo_likelihood.py
+++ likelihood/tdcosmo/tdcosmo_likelihood.py
@@ -2,23 +2,18 @@ import os
 import pickle
 from cosmosis.datablock import names, SectionOptions
 from hierarc.Likelihood.lens_sample_likelihood import LensSampleLikelihood
-from astropy.cosmology import LambdaCDM
+from astropy.cosmology import w0waCDM
 from lenstronomy.Cosmo.cosmo_interp import CosmoInterp
 from scipy.interpolate import interp1d
+
 
 class TDCOSMOlenses:
     def __init__(self, options):
         dir_path = os.path.dirname(__file__)
 
-        #todo: unpack options to select the cosmology. Default is LCDM
-        self.cosmo_model = options.get_string("cosmo_model", default='LambdaCDM')
         self.data_sets = options.get_string("data_sets", default='tdcosmo7')
-        self.num_distribution_draws = options.get_string("num_distribution_draws", default=200)
-
-        if self.cosmo_model == 'LambdaCDM':
-            self.cosmology = LambdaCDM
-        else:
-            raise NotImplementedError()
+        self._num_distribution_draws = options.get_int("num_distribution_draws", default=200)
+        self._distances_computation_module = options.get_string("distances_computation_module", default='astropy')
 
         # 7 TDCOSMO lenses
         file = open(os.path.join(dir_path, 'tdcosmo7_likelihood_processed.pkl'), 'rb')
@@ -37,11 +32,11 @@ class TDCOSMOlenses:
 
         # here we update each individual lens likelihood configuration with the setting of the Monte-Carlo marginalization over hyper-parameter distributions
         for lens in tdcosmo7_likelihood_processed:
-            lens['num_distribution_draws'] = self.num_distribution_draws
+            lens['num_distribution_draws'] = self._num_distribution_draws
         for lens in slacs_sdss_likelihood_processed:
-            lens['num_distribution_draws'] = self.num_distribution_draws
+            lens['num_distribution_draws'] = self._num_distribution_draws
         for lens in slacs_ifu_likelihood_processed:
-            lens['num_distribution_draws'] = self.num_distribution_draws
+            lens['num_distribution_draws'] = self._num_distribution_draws
 
         # ====================
         # TDCOSMO 7 likelihood
@@ -56,11 +51,12 @@ class TDCOSMOlenses:
         if 'SLACS_IFU' in self.data_sets:
             lens_list += slacs_ifu_likelihood_processed
 
-        assert len(lens_list)>0, "Data not found ! Add at least one of those 3 data sets 'tdcosmo7', 'SLACS_SDSS' or 'SLACS_IFU'"
-        #choose which likelihood you want here:
-        self.likelihood = LensSampleLikelihood(lens_list)
+        assert len(
+            lens_list) > 0, "Data not found ! Add at least one of those 3 data sets 'tdcosmo7', 'SLACS_SDSS' or 'SLACS_IFU'"
+        # choose which likelihood you want here:
+        self._likelihood = LensSampleLikelihood(lens_list)
 
-        #choose if you want the full astropy distance calculation or a interpolated version of it (for speed-up)
+        # choose if you want the full astropy distance calculation or a interpolated version of it (for speed-up)
         self._interpolate_distances_type = 'None'
 
     def cosmosis_cosmo_2_astropy_cosmo(self, block):
@@ -69,66 +65,95 @@ class TDCOSMOlenses:
         :param cosmosis_cosmo: cosmosis cosmology object
         :return ~astropy.cosmology equivalent cosmology object
         """
-        h = block['cosmological_parameters', 'h0']
+        H0 = block['cosmological_parameters', 'h0'] * 100
         om = block['cosmological_parameters', 'omega_m']
-        ol = block['cosmological_parameters', 'omega_l'] #todo: check this, not sure how parameters are named
+        ok = block['cosmological_parameters', 'omega_k']
+        ob = block['cosmological_parameters', 'omega_b']
+        w0 = block['cosmological_parameters', 'w']
+        wa = block['cosmological_parameters', 'wa']
+        mnu = block['cosmological_parameters', 'mnu']
+        nnu = block['cosmological_parameters', 'nnu']
 
-        if self._interpolate_distances_type == 'CosmoInterp':
-            cosmo = self.cosmology(H0=h, Om0=om, Ode0=ol)
+        ol = 1 - om - ok
+
+        if self._distances_computation_module == 'astropy':
+            # we are using standard astropy cosmology for distance compuation
+            cosmo = w0waCDM(H0=H0, Om0=om, Ode0=ol, Ob0=ob, w0=w0, wa=wa, m_nu=mnu, Neff=nnu)
+        elif self._distances_computation_module == 'CosmoInterp':
+            # we are using an interpolated version of the standard astropy cosmology (for speed-up)
+            cosmo = w0waCDM(H0=H0, Om0=om, Ode0=ol, Ob0=ob, w0=w0, wa=wa, m_nu=mnu, Neff=nnu)
             cosmo = CosmoInterp(cosmo=cosmo, z_stop=5, num_interp=100)
-        elif self._interpolate_distances_type == 'None':
-            #we are falling back to astropy cosmology (might be slow)
-            cosmo = self.cosmology(H0=h, Om0=om, Ode0=ol)
-        elif self._interpolate_distances_type == 'cosmosis':
-            #use the distance redshift relation of cosmosis, might be the fastest
+        elif self._distances_computation_module == 'camb':
+            # todo: need to overwride the astropy.cosmology class with a custom set of distance functions see the class commented at the end of this file
+
+            # we use the camb distances
             # z_bg = block['distances', 'z']
             # D_A = block['distances', 'D_A']
-            angular_distance = interp1d(z_bg, D_A, kind='linear')
-            # todo to be implemented as in the holicow module, might need to ovewride the astropy.cosmology class with a custom set of distance funcitons
+            # cosmo = CustomCosmo(z_bg, D_A)
             raise NotImplementedError()
         else:
             raise ValueError()
 
         return cosmo
 
-
     def likelihood(self, block):
         cosmo = self.cosmosis_cosmo_2_astropy_cosmo(block)
 
         # here the additional parameters required to evaluate the likelihood in accordance with TDCOSMO IV Table 3
-        lambda_mst = block['nuisance_lensing','lambda_mst']
-        lambda_mst_sigma = block['nuisance_lensing','lambda_mst_sigma']
-        alpha_lambda = block['nuisance_lensing','alpha_lambda']
-        a_ani = block['nuisance_lensing','a_ani']
-        a_ani_sigma = block['nuisance_lensing','a_ani_sigma']
+        lambda_mst = block['nuisance_lensing', 'lambda_mst']
+        lambda_mst_sigma = block['nuisance_lensing', 'lambda_mst_sigma']
+        alpha_lambda = block['nuisance_lensing', 'alpha_lambda']
+        a_ani = block['nuisance_lensing', 'a_ani']
+        a_ani_sigma = block['nuisance_lensing', 'a_ani_sigma']
 
         kwargs_lens_test = {'lambda_mst': lambda_mst,  # mean in the internal MST distribution
-                        'lambda_mst_sigma': lambda_mst_sigma,  # Gaussian sigma of the distribution of lambda_mst
-                        'alpha_lambda': alpha_lambda,  # slope of lambda_mst with r_eff/theta_E
-                       }
+                            'lambda_mst_sigma': lambda_mst_sigma,  # Gaussian sigma of the distribution of lambda_mst
+                            'alpha_lambda': alpha_lambda,  # slope of lambda_mst with r_eff/theta_E
+                            }
         kwargs_kin_test = {'a_ani': a_ani,  # mean a_ani anisotropy parameter in the OM model
-                       'a_ani_sigma': a_ani_sigma,  # sigma(a_ani)⟨a_ani⟩ is the 1-sigma Gaussian scatter in a_ani
-                      }
+                           'a_ani_sigma': a_ani_sigma,  # sigma(a_ani)⟨a_ani⟩ is the 1-sigma Gaussian scatter in a_ani
+                           }
 
-        #Default value, for reference
-        # kwargs_lens_test = {'lambda_mst': 1.,  # mean in the internal MST distribution
-        #                 'lambda_mst_sigma': 0.04,  # Gaussian sigma of the distribution of lambda_mst
-        #                 'alpha_lambda': 0,  # slope of lambda_mst with r_eff/theta_E
-        #                }
-        # kwargs_kin_test = {'a_ani': 1.5,  # mean a_ani anisotropy parameter in the OM model
-        #                'a_ani_sigma': 0.3,  # sigma(a_ani)⟨a_ani⟩ is the 1-sigma Gaussian scatter in a_ani
-        #               }
-
-        logl = self.likelihood.log_likelihood(cosmo=cosmo, kwargs_lens=kwargs_lens_test, kwargs_kin=kwargs_kin_test)
+        logl = self._likelihood.log_likelihood(cosmo=cosmo, kwargs_lens=kwargs_lens_test, kwargs_kin=kwargs_kin_test)
 
         return float(logl)
+
 
 def setup(options):
     options = SectionOptions(options)
     return TDCOSMOlenses(options)
+
 
 def execute(block, config):
     like = config.likelihood(block)
     block[names.likelihoods, "TDCOSMO_like"] = like
     return 0
 
+# todo : ere implement a custom class that have the same method as astropy cosmology, but overwrite the angular diameter computation
+# class CustomCosmo(w0waCDM):
+#     def __init__(z_bkg, D_A):
+#         self.z_bkg = z_bkg
+#         self.D_A = D_A
+#         self.angular_distance = np.interp1d(self.z_bkg, self.D_A, kind='linear')
+#     def Da(z):
+#           return self.angular_distance(z)
+#     def calculate_Dds(self, ok, K, zd, zs, Dd, Ds):
+#
+#         # to compute Dds from Dd and Ds, first need to figure out whether the universe is flat
+#
+#         if (np.fabs(ok) < 1.e-6):  # flat Universe, so can use simple formula
+#             Dds = ((1. + zs) * Ds - (1 + zd) * Dd) / (1. + zs)
+#
+#         elif (K > 0):
+#             chis = np.arcsin(Ds * (1. + zs) * np.sqrt(K)) / np.sqrt(K)
+#             chid = np.arcsin(Dd * (1. + zd) * np.sqrt(K)) / np.sqrt(K)
+#             chids = chis - chid
+#             Dds = (1. / (1 + zs)) * (1. / np.sqrt(K)) * np.sin(np.sqrt(K) * chids)
+#
+#         else:  # K<0
+#             chis = np.arcsinh(Ds * (1. + zs) * np.sqrt(-K)) / np.sqrt(-K)
+#             chid = np.arcsinh(Dd * (1. + zd) * np.sqrt(-K)) / np.sqrt(-K)
+#             chids = chis - chid
+#             Dds = (1. / (1 + zs)) * (1. / np.sqrt(-K)) * np.sinh(np.sqrt(-K) * chids)
+#
+#         return Dds
