bde7ced5b2c9617d744ef30e965424a80553c744
Agnès Ferté <ferte@slac.stanford.edu>
2023-12-08 22:00:18 +0100

    Add xlens at the level of 2pt.

--- modules_desy6/add_xlens_2pt.py
+++ modules_desy6/add_xlens_2pt.py
@@ -2,9 +2,25 @@ from cosmosis.datablock import names, option_section
 import numpy as np
 
 def setup(options):
-    # Get the name of the bias section
-    bias_section = options.get_string(option_section, "bias_section", "bias_lens")
 	# Get the name of the xlens section
     r_section = options.get_string(option_section,"r_section", "x_lens")
 
     return bias_section, r_section
+
+
+def execute(block, config):
+
+    r_section = config
+
+    rval = block[r_section, 'rmean_bin']
+
+
+    for i in range(1,6):
+        for j in range(1,6):
+            if j >= i:
+                ggl_bin_label = 'bin_'+str(i)+'_'+str(j)
+                ggl = block['galaxy_shear_xi',ggl_bin_label]
+                ggl_xlens = rval*ggl
+                block['galaxy_shear_xi', ggl_bin_label] = ggl_xlens
+
+    return 0
