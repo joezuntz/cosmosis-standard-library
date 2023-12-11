d426b80aa96975c4e646037fc5f184702f484cdb
Agnès Ferté <ferte@slac.stanford.edu>
2023-12-11 10:35:03 +0000

    Update xlens module.

--- modules_desy6/add_xlens_2pt.py
+++ modules_desy6/add_xlens_2pt.py
@@ -3,24 +3,24 @@ import numpy as np
 
 def setup(options):
 	# Get the name of the xlens section
-    r_section = options.get_string(option_section,"r_section", "x_lens")
+    x_section = options.get_string(option_section,"xlens", "xlens")
 
-    return bias_section, r_section
+    return x_section
 
 
 def execute(block, config):
 
-    r_section = config
+    x_section = config
 
-    rval = block[r_section, 'rmean_bin']
+    xlens_val = block[x_section, 'xlens_all']
 
 
     for i in range(1,6):
-        for j in range(1,6):
+        for j in range(1,4):
             if j >= i:
                 ggl_bin_label = 'bin_'+str(i)+'_'+str(j)
                 ggl = block['galaxy_shear_xi',ggl_bin_label]
-                ggl_xlens = rval*ggl
+                ggl_xlens = xlens_val*ggl
                 block['galaxy_shear_xi', ggl_bin_label] = ggl_xlens
 
     return 0
