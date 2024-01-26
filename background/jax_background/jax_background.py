4e21e93ae6ff966bb7f04bf4b6715ab05325eb7a
Joe Zuntz <joezuntz@googlemail.com>
2024-01-26 13:31:50 +0000

    jax fixes

--- background/jax_background/jax_background.py
+++ background/jax_background/jax_background.py
@@ -27,9 +27,10 @@ def execute(inputs, config):
     omega_b = 0.044
     omega_c = omega_m - omega_b
     cosmo = jax_cosmo.Cosmology(Omega_c=omega_c, Omega_b=omega_b, h=h0, n_s=0.96, sigma8=0.8, Omega_k=omega_k, w0=w, wa=0.0)
-    z = jnp.linspace(0.0, config["zmax"], config["nz"])[1:]
+    z = jnp.linspace(0.0, config["zmax"], config["nz"])
     a = 1 / (1 + z)
     d_a = jax_cosmo.background.angular_diameter_distance(cosmo, a) / h0
+    d_m = jax_cosmo.background.transverse_comoving_distance(cosmo, a) / h0
     d_l = (1 + z)**2 * d_a
     mu = 5 * jnp.log10(d_l) + 25
     return {
@@ -37,5 +38,6 @@ def execute(inputs, config):
         ("distances", "a"): a,
         ("distances", "mu"): mu,
         ("distances", "d_a"): d_a,
+        ("distances", "d_m"): d_m,
         ("distances", "d_l"): d_l,
     }
