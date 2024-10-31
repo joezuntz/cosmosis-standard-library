0db52ca21cd429a4ad90b1be3c11e5aa78be3d3b
Aaron Ouellette <aaronjo2@illinois.edu>
2024-10-30 20:25:25 -0500

    some updates for the amod module

--- structure/amod/amod.py
+++ structure/amod/amod.py
@@ -12,8 +12,10 @@ def execute(block):
     A_mod = block["amod_parameter", "a_mod"]
 
     # for now assume pk_lin and pk_nonlin are defined on the same grids
-    assert (block["matter_power_lin", "z"] == block["matter_power_nl", "z"]).all()
-    assert (block["matter_power_lin", "k_h"] == block["matter_power_nl", "k_h"]).all()
+    z_same = np.allclose(block["matter_power_lin", "z"] == block["matter_power_nl", "z"])
+    k_same = np.allclose(block["matter_power_lin", "k_h"] == block["matter_power_nl", "k_h"])
+    if not (z_same and k_same):
+        raise ValueError("The z and k sampling of the linear and non-linear matter power spectra must be the same for the amod module")
 
     # calculate new non-linear power
     pk_m = pk_lin + A_mod * (pk_nonlin - pk_lin)
