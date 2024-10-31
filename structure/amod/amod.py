e5e3eed9d1219ed70463a51aa05660d7ce6581f1
Aaron Ouellette <aaronjo2@illinois.edu>
2024-10-30 22:06:55 -0500

    fix typo

--- structure/amod/amod.py
+++ structure/amod/amod.py
@@ -13,8 +13,8 @@ def execute(block):
     A_mod = block["amod_parameter", "a_mod"]
 
     # for now assume pk_lin and pk_nonlin are defined on the same grids
-    z_same = np.allclose(block["matter_power_lin", "z"] == block["matter_power_nl", "z"])
-    k_same = np.allclose(block["matter_power_lin", "k_h"] == block["matter_power_nl", "k_h"])
+    z_same = np.allclose(block["matter_power_lin", "z"], block["matter_power_nl", "z"])
+    k_same = np.allclose(block["matter_power_lin", "k_h"], block["matter_power_nl", "k_h"])
     if not (z_same and k_same):
         raise ValueError("The z and k sampling of the linear and non-linear matter power spectra must be the same for the amod module")
 
