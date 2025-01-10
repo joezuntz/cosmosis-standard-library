0b91f9439c750e24e964a86af9d8f5ca72fca231
Michael Troxel <mat90@dcc-login-05.oit.duke.edu>
2025-01-10 11:50:56 -0500

    update uz per bin ability

--- modules_desy6/photoz_uz/photoz_uz.py
+++ modules_desy6/photoz_uz/photoz_uz.py
@@ -4,26 +4,39 @@ import numpy as np
 
 def setup(options):
     sample = options.get_string(option_section, "sample", "")
+    pz = 'nz_'+config['sample']
     bias_section = sample+"_photoz_u"
     basis_file = options.get_string(option_section, "basis_file", "")
     n_modes = options.get_int(option_section, "n_modes", 0)
-    U = np.loadtxt(basis_file)[:,:n_modes]
+    z = block[pz, "z"]
+    U = np.loadtxt(basis_file)[:,:n_modes].reshape((n_modes,block[pz, "nbin"],len(z)))
+    perbin = options.get_string(option_section, "perbin", False)
+    rescale = options.get_string(option_section, "rescale", False)
     return {"sample": sample,
             "bias_section": bias_section,
             "basis":U,
-            "n_modes":n_modes}
+            "n_modes":n_modes,
+            "rescale":rescale,
+            "perbin":perbin}
 
 def execute(block, config):
     pz = 'nz_'+config['sample']
     uvals = config['bias_section']
+    if rescale:
+        # replace below line with call to rescale the values from an initial unit gaussian.
+        # Otherwise it will use the gaussian prior provided in the cosmosis file
+        # Values should have names ...
+        uvals=uvals
     U = config['basis']
     n_modes = config['n_modes']
     nbin = block[pz, "nbin"]
-    if nbin>4:
-        nbin=4
     z = block[pz, "z"]
-    dz = U@np.array([block[uvals, "u_%d" % j] for j in range(n_modes)])
-    dz = dz.reshape((nbin,len(dz)//nbin))
+    dz = np.zeros((nbins,len(z)))
+    for i in range(1,nbin+1):
+        if perbin:
+            dz[i-1,:] = U[:,i-1,:]@np.array([block[uvals, "u_{0}_{1}".format(i-1,j) ] for j in range(n_modes)])
+        else:
+       	    dz[i-1,:] = U[:,i-1,:]@np.array([block[uvals, "u_{0}".format(j) ] for j in range(n_modes)])
     for i in range(1,nbin+1):
         bin_name = "bin_%d" % i
         nz = block[pz, bin_name]
