b81168bdac3f2a49d4f5a63f3601be0d5a4b269f
wdoumerg <89606153+wdoumerg@users.noreply.github.com>
2025-01-13 16:36:20 +0100

    Add degaussianization in  photoz_uz.py
    Add class Normalizer_cosmosis
    In setup:
    -Add reading of the Percentiles.txt files
    -Define The set of classes Percentiles_classes, and add them to the dictionary
    
    In  the function: 
    -Read the classes Percentiles_classes
    - in the if, degauss all the u_ according to the format given in the comments.

--- modules_desy6/photoz_uz/photoz_uz.py
+++ modules_desy6/photoz_uz/photoz_uz.py
@@ -2,19 +2,47 @@ from builtins import range
 from cosmosis.datablock import option_section, names
 import numpy as np
 
+class Normalizer_cosmosis:
+    def __init__(self,percentile, gmax=5,xmax=10, kind='linear'):
+        '''Class to create transformations from the gaussian 
+        to the non-gaussian u distribution
+        input: the percentile array of the initial u array
+        generated with the same gmax, dg convention, 
+        The methods  `degauss` will do  g->u.
+        '''
+        # Establishing matching percentile points
+        dg = 0.1
+        gg = np.arange(-gmax,gmax+dg/2, dg)
+        xx = np.copy(percentile)
+        # Clean out any duplicates in the tables.  This is hacky.
+        dup = xx[1:]==xx[:-1]
+        keep = np.concatenate( (np.logical_not(dup), [True]))
+        gg = gg[keep]
+        xx = xx[keep]
+        self.inv = interp1d(gg,xx,kind=kind, bounds_error=False, fill_value=(-xmax,xmax))
+        return
+    def degauss(self,g):
+        '''Return de-Gaussianized values of g'''
+        return self.inv(g)
+
+
 def setup(options):
     sample = options.get_string(option_section, "sample", "")
     pz = 'nz_'+config['sample']
     bias_section = sample+"_photoz_u"
     basis_file = options.get_string(option_section, "basis_file", "")
+    percentile_file = options.get_string(option_section, "Percentiles", "")
     n_modes = options.get_int(option_section, "n_modes", 0)
     z = block[pz, "z"]
     U = np.loadtxt(basis_file)[:,:n_modes].reshape((n_modes,block[pz, "nbin"],len(z)))
+    Percentiles = np.loadtxt(percentile_file)[:,:n_modes].reshape((n_modes,block[pz, "nbin"],101))
+    Percentiles_classes=[[Normalizer_cosmosis(Percentiles[jmode][ibin]) for jmode in range(n_modes)] for ibin in range(block[pz, "nbin"])]
     perbin = options.get_string(option_section, "perbin", False)
     rescale = options.get_string(option_section, "rescale", False)
     return {"sample": sample,
             "bias_section": bias_section,
             "basis":U,
+            "Percentiles_classes":Percentiles_classes,
             "n_modes":n_modes,
             "rescale":rescale,
             "perbin":perbin}
@@ -22,6 +50,7 @@ def setup(options):
 def execute(block, config):
     pz = 'nz_'+config['sample']
     U = config['basis']
+    Percentiles_classes = config['Percentiles_classes']
     n_modes = config['n_modes']
     nbin = block[pz, "nbin"]
     z = block[pz, "z"]
@@ -37,7 +66,10 @@ def execute(block, config):
         # Otherwise it will use the gaussian prior provided in the cosmosis file.
         # u_ is an array of the values for each bin with shape (nbins,n_modes).
         # if perbin=False, then the nbins dimension is just repeated entries.
-        u_=u_
+        #u_=u_
+        for ibin in range(nbin):
+            for jmode in range(n_modes):
+                u_[ibin,jmode]=  Percentiles_classes[ibin][jmode].degauss(u_[ibin,jmode])
     for i in range(1,nbin+1):
         dz = U[:,i-1,:]@u_[i-1,:]
         bin_name = "bin_%d" % i
