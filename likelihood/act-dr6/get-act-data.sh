3de3aee32530443e659810f8a35119f8189882c6
Joe Zuntz <joezuntz@googlemail.com>
2025-03-25 12:27:25 +0000

    another go at script

--- likelihood/act-dr6/get-act-data.sh
+++ likelihood/act-dr6/get-act-data.sh
@@ -8,7 +8,7 @@ else
     wget https://portal.nersc.gov/project/act/dr6_data/dr6_data.tar.gz
     tar -zxvf dr6_data.tar.gz
     mkdir -p data/ACTDR6MFLike/v1.0
-    mv dr6_data.fits data/ACTDR6MFLike/v1.0
+    mv v1.0/dr6_data.fits data/ACTDR6MFLike/v1.0
     rm dr6_data.tar.gz
 fi
 popd
