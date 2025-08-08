a628e8acf9f7ad378d1fa3b2a7c7a87b7f2da599
Ian Harrison <itrharrison@gmail.com>
2025-08-08 12:20:46 +0100

    documentation for lite likelihood

--- likelihood/act-dr6-lite/act_dr6_lite_interface.py
+++ likelihood/act-dr6-lite/act_dr6_lite_interface.py
@@ -1,7 +1,7 @@
 try:
     import act_dr6_cmbonly
 except ImportError:
-    raise RuntimeError('The act_dr6_cmbonly python module is required for the act_dr6_lite likelihood.')
+    raise RuntimeError('The act_dr6_cmbonly python module is required for the act_dr6_lite likelihood. It can be obtained from https://github.com/ACTCollaboration/DR6-ACT-lite')
 from cosmosis.datablock import names
 cosmo = names.cosmological_parameters
 import numpy as np
